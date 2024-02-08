088a99a42f5bbe4025990aa83342854d186ea902
Molina-Q <quentinmolina1@gmail.com>
2024-02-08 08:34:59 +0100

    update Modal & Map component

---
+++ assets/react/controllers/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LeafletMap from "./components/LeafletMap";
+import Modal from "./components/modal/ModalShowArticle";
+
+const App = () => {
+
+
+    return ( <>
+        <LeafletMap />
+    </>
+    )
+}
+
+export default App
+  
--- assets/react/controllers/components/LeafletMap.jsx
+++ assets/react/controllers/components/LeafletMap.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect, useState, useMemo } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap, Polygon, SVGOverlay, useMapEvent, useMapEvents, ImageOverlay, GeoJSON } from 'react-leaflet';
-import myGeoJson from '../../../public/geojson/countries.json';
+import myGeoJson from '../../../../public/geojson/countries.json';
+import Modal from './modal/ModalShowArticle';
 
 const LeafletMap = () => {
-  // const [geoJSONData, setGeoJSONData] = useState(null);
   const position = [46.2276, 2.2137];
   const geoJsonFeatures = myGeoJson.features;
 
@@ -17,6 +17,24 @@ const LeafletMap = () => {
     ESP: {color: "orange", fillOpacity: 1},
   };
 
+  // variable with state of dialog and open or setOpen
+  const [openModal, setOpenModal] = useState(true)
+
+  const ModalIsClosed = () => {
+    if (openModal) {
+      setOpenModal(false)
+    }
+  }
+//   useEffect(() => {
+//     const modalState = modalRef.current;
+//     if (modalElement) {
+//         if (isModalOpen) {
+//             modalElement.showModal();
+//         } else {
+//             modalElement.close();
+//         }
+//     }
+// }, [isModalOpen]);
   /**
    * Function that loops on all of the countries in the json sent in params and return them as an array
    */
@@ -28,7 +46,7 @@ const LeafletMap = () => {
 
     for (let feature of jsonFeatures) {
       // console.log(options)
-      console.log("feature.geometry.coordinates = ", feature.geometry.coordinates);
+      // console.log("feature.geometry.coordinates = ", feature.geometry.coordinates);
       let polygon;
 
       // if simple polygon
@@ -46,8 +64,9 @@ const LeafletMap = () => {
       const handlers = useMemo(
         () => ({
           click() {
-            setBounds(polygon)
-            map.flyToBounds(polygon) //panInsideBounds
+            // setBounds(polygon)
+            map.panInsideBounds(polygon)
+            setOpenModal(true)
           },
         }),
         [map],
@@ -60,8 +79,6 @@ const LeafletMap = () => {
           key={feature.properties.ADMIN}  // their names are used as key
           pathOptions={options[feature.properties.ISO_A3]} // change the style variable depending on the country
           eventHandlers={handlers}
-
-          className={"qqch"}
         />
       );
     }
@@ -69,67 +86,27 @@ const LeafletMap = () => {
     return geoJSON;
   }
 
-  
-
-  // function LocationMarker() {
-  //   const [position, setPosition] = useState(null)
-  //   const map = useMapEvents({
-  //     click() {
-  //       map.locate()
-  //     },
-  //     locationfound(e) {
-  //       setPosition(e.latlng)
-  //       map.flyTo(e.latlng, map.getZoom())
-  //     },
-  //   })
-  
-  //   return position === null ? null : (
-  //     <Marker position={position}>
-  //       <Popup>You are here</Popup>
-  //     </Marker>
-  //   )
-  // }
-
-
-  // function SetBoundsRectangles() {
-  //   const [bounds, setBounds] = useState(outerBounds)
-  //   const map = useMap()
-  
-  //   const innerHandlers = useMemo(
-  //     () => ({
-  //       click() {
-  //         setBounds(innerBounds)
-  //         map.fitBounds(innerBounds)
-  //       },
-  //     }),
-  //     [map],
-  //   )
-  //   const outerHandlers = useMemo(
-  //     () => ({
-  //       click() {
-  //         setBounds(outerBounds)
-  //         map.fitBounds(outerBounds)
-  //       },
-  //     }),
-  //     [map],
-  //   )
-  // }
-
-  return (
+  return (<>
     <MapContainer className='map' center={position} zoom={6} scrollWheelZoom={true} >
-
+      {/* My map img */}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       
+      {/* My map img */}
       <GeoJsonGeometry jsonFeatures={geoJsonFeatures} />
+ 
+    </MapContainer>   
 
-      {/* <GeoJSON data={myGeoJson.features[0].geometry} /> */}
-      {/* {geoJSONData && <GeoJSON data={geoJSONData} ref={geoJSONRef} />} */}
+    <Modal 
+      isOpen={openModal}
+      hasCloseBtn={true}
+      onClose={ModalIsClosed}
+      children={'Bonjour à tous'}
+    />
 
-    </MapContainer>
-  );
+  </>);
 };
 
 
