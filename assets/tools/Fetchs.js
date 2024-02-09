b78124c159f16ec10fd52d522cdea877fdb7ebe3
Molina-Q <quentinmolina1@gmail.com>
2024-02-09 15:46:01 +0100

    directory with JS functions used inside React

---
+++ assets/tools/Fetchs.js
@@ -0,0 +1,20 @@
+
+// fonction principale qui permet de faire un fetch
+exports.fetchAnything = async function fetchAnything(uri) {
+
+    try {
+        const response = await fetch(uri);
+
+        if (response.ok) { // .success || .ok
+            const data = await response.json();
+
+            console.log("fetchAnything() - data = ", data);
+
+            return data;
+        }
+    } catch(error) {
+        console.error(error);
+    }
+
+    return null;
+  }
