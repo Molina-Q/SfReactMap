45ad32c09d8a6912617e9aa2be6fcd7885467ffe
Molina-Q <quentinmolina1@gmail.com>
2024-02-14 15:04:31 +0100

    Fixed issue of state reset and removed unnecessary function

--- assets/react/controllers/components/timeline/Timeline.jsx
+++ assets/react/controllers/components/timeline/Timeline.jsx
@@ -7,50 +7,58 @@ import React, { useRef, useEffect, useState } from "react";
  * @returns 
  */
 const Timeline = ({ defaultYear = '1900', returnChecked }) => {
-    const checkedDefault = defaultYear ;
-    // console.log(defaultYear.defaultYear)
 
 
-    const [CheckedRadio, setCheckedRadio] = useState(checkedDefault);
+    const [defaultChecked, setDefaultChecked] = useState(defaultYear);
+    const [checkedRadio, setCheckedRadio] = useState(defaultYear);
+
+    // useEffect(() => {
+    //     setCheckedRadio(defaultChecked);
+    // }, []);
 
     const periods = ['1400', '1500', '1600', '1700', '1800', '1900'];
     
-    // called when a new radio is clicked 
+    // // called when a new radio is clicked 
     const handleChange = e => {
+        // set it with the value of the clicked radio
         setCheckedRadio(e.target.value);
-        returnChecked(e.target.value);
+        // returnChecked(e.target.value);
     }
     
-    // console.log('Current checked radio', CheckedRadio);
+    // callback used by parent sfReactMap to get the value of the clicked radio
+    useEffect(() => {
+        returnChecked(checkedRadio); 
+    }, [checkedRadio])
 
     /**
     * Function that loops on all of the periods sent in params and return a collection of radio inputs
     */
-    const Radiobox = () => {
-        const elements = []; // array that will have every input radio of the timeline
+    // deleted the original function because it was creating a bug that reseted the component after a click(and was completely unnecessary)
+    // Note for the future : never create a component inside a component !!!
+    
+    const elements = []; // array that will have every input radio of the timeline
 
-        for (let period of periods) {
+    for (let period of periods) {
 
-            // push each country's geometry in the array, and use their names as key
-            elements.push(<div key={period}>
+        // push each country's geometry in the array, and use their names as key
+        elements.push(
+            <div key={period}>
                 <input 
                     type="radio" 
                     name="timelineYear"  
                     value={period} 
                     id={period} 
                     onChange={handleChange} 
-                    defaultChecked={period === CheckedRadio ? true : false}
+                    defaultChecked={period === defaultChecked}
                 />
                 <label htmlFor={period}>{period}</label>
-              </div>
-            );
+            </div>
+        );
 
-        }
+    }
 
-        return elements;
-    };
-
-    return <Radiobox />;
+    return elements;
+  
 }   
 
 export default Timeline;
