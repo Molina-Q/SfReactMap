8752900e12be5ae8cfb5cc325d890b50558d05c4
Molina-Q <quentinmolina1@gmail.com>
2024-02-14 16:28:26 +0100

    Update method to be fetched with serializer and circular references

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class MapController extends AbstractController
@@ -27,27 +29,40 @@ class MapController extends AbstractController
         string $country,
         string $century,
         ArticleRepository $articleRepository,
+        SerializerInterface $serializer,
     ): Response
     {
+        $encoder = new JsonEncoder();
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function (object $object, string $format, array $context): string {
+                return $object->getId();
+            },
+        ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        
+        $serializer = new Serializer([$normalizer], [$encoder]);
 
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
+        // {"name":"Les-Tilleuls.coop","members":[{"name":"K\u00e9vin", organization: "Les-Tilleuls.coop"}]}
+        // $encoders = [new XmlEncoder(), new JsonEncoder()];
+        // $normalizers = [new ObjectNormalizer()];
 
-        $serializer = new Serializer($normalizers, $encoders);
+        // $serializer = new Serializer($normalizers, $encoders);
+
         // $article = $articleRepository->findOneByCountry($country);
         $article = $articleRepository->findOneByCountry($country, $century);
         // $articleJSON = json_encode($article);
 
+        $jsonContent = $serializer->serialize($article, 'json');
+
         if(!$article) {
-            $dataCountry = "Sorry but this country doesn't have an Article for this period";
-            return new JsonResponse(['title' => $dataCountry]);
+            $dataCountry = "Sorry this country doesn't have an Article for this period.";
+            return new JsonResponse(false);
         }
 
-        $jsonContent = $serializer->serialize($article->getCountry(), 'json');
 
         return new JsonResponse([ 
-            'id_article' => $article->getId(),
-            'article' => $jsonContent
+            // 'id_article' => $article->getId(),
+            'article' => \json_decode($jsonContent)
             // 'title' => $article->getTitle(),
             // 'summary' => $article->getSummary(),
             // 'country' => $article->getCountry()->getName(),
