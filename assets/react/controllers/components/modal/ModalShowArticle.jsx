7920c63cc9b9d88dc31625344b1a72b9927f0497
Molina-Q <quentinmolina1@gmail.com>
2024-02-19 16:22:03 +0100

    code cleanup

--- assets/react/controllers/components/modal/ModalShowArticle.jsx
+++ assets/react/controllers/components/modal/ModalShowArticle.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 
-const ModalShowArticle = ({ isOpen, onClose, children }) => {
+const ModalShowArticle = ({ isOpen, onClose, handleReturn, children }) => {
     // console.log('echo Modal');
 
     const [isModalOpen, setModalOpen] = useState(isOpen); // make the variable dynamic and change without reload
@@ -46,6 +46,11 @@ const ModalShowArticle = ({ isOpen, onClose, children }) => {
             <button type="button" className="modal-close-btn" onClick={handleCloseModal} > 
                 <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.*/}<path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
             </button>
+
+            <button type="button" className="modal-return-arrow" onClick={handleReturn}>
+                <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{/*Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.*/}<path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+            </button>
+            
             {/* this is props.children this will show everything written inside the component element in the top component calling it */}
             {children}
 
