51a0cd6a9ff3712cd88a09ab903c26e8f0c3e3c2
Molina-Q <quentinmolina1@gmail.com>
2024-02-19 16:22:38 +0100

    added method to get section's information

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Repository\SectionRepository;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MapController extends AbstractController
 {
+    // the index 
     #[Route('/map', name: 'app_map')]
     public function index(): Response
     {
@@ -24,7 +26,37 @@ class MapController extends AbstractController
         ]);
     }
 
-    #[Route('/dataCountry/{country}/{century}', name: 'show_count')]
+    #[Route('/dataCountry/section/{id}', name: 'show_section', methods: ['GET'])]
+    public function showSection(
+        int $id,
+        SectionRepository $sectionRepository,
+    ): Response
+    {
+
+
+        $encoder = new JsonEncoder();
+
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function (object $object, string $format, array $context): string {
+                return $object->getId();
+            },
+        ];
+
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        
+        $serializer = new Serializer([$normalizer], [$encoder]);
+        
+        $section = $sectionRepository->findOneById($id);
+
+        $jsonContent = $serializer->serialize($section, 'json');
+
+        return new JsonResponse([ 
+            'section' => \json_decode($jsonContent)
+        ]);
+
+    }
+
+    #[Route('/dataCountry/{country}/{century}', name: 'show_count', methods: ['GET'])]
     public function dataCount(
         string $country,
         string $century,
@@ -32,25 +64,20 @@ class MapController extends AbstractController
         SerializerInterface $serializer,
     ): Response
     {
+
         $encoder = new JsonEncoder();
+
         $defaultContext = [
             AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function (object $object, string $format, array $context): string {
                 return $object->getId();
             },
         ];
+
         $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
         
         $serializer = new Serializer([$normalizer], [$encoder]);
 
-        // {"name":"Les-Tilleuls.coop","members":[{"name":"K\u00e9vin", organization: "Les-Tilleuls.coop"}]}
-        // $encoders = [new XmlEncoder(), new JsonEncoder()];
-        // $normalizers = [new ObjectNormalizer()];
-
-        // $serializer = new Serializer($normalizers, $encoders);
-
-        // $article = $articleRepository->findOneByCountry($country);
         $article = $articleRepository->findOneByCountry($country, $century);
-        // $articleJSON = json_encode($article);
 
         $jsonContent = $serializer->serialize($article, 'json');
 
@@ -59,22 +86,13 @@ class MapController extends AbstractController
             return new JsonResponse(false);
         }
 
-
+        // return $this->json($jsonContent, 200, [], [''])
         return new JsonResponse([ 
-            // 'id_article' => $article->getId(),
             'article' => \json_decode($jsonContent)
-            // 'title' => $article->getTitle(),
-            // 'summary' => $article->getSummary(),
-            // 'country' => $article->getCountry()->getName(),
-            // 'century' => $article->getCentury()->getYear(),
-            
-            // 'sectionsTitle' => $jsonSection,
-            // 'sectionsContent' => $article->getSections(),
         ]);
 
-        // return new JsonResponse(json_encode([
-        //     "article" => $jsonContent,
-        // ]));
     }
+
+
  
 }
