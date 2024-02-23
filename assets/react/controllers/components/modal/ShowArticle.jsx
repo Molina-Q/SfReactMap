3c3807662f03db5a563b0deef93fe0d18f627846
Molina-Q <quentinmolina1@gmail.com>
2024-02-23 11:49:13 +0100

    Created ShowArticle component

---
+++ assets/react/controllers/components/modal/ShowArticle.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import SingleSectionInArticle from './SingleSectionInArticle'
+
+function ShowArticle({article, showDetails}) {
+  return (
+    <article>
+
+        <h2>{article.Country.name} - {article.Century.year}</h2>
+
+        <h1>{article.title}</h1> 
+
+        <p>{article.summary}</p> 
+
+        <section className='section-container'>
+          
+          {article.sections.map((section) => (
+            <SingleSectionInArticle key={section.id} section={section} showDetails={showDetails} />
+          ))}
+
+        </section>
+
+    </article> 
+  )
+}
+
+export default ShowArticle
