11a3ba1312f6301694331b03aea296107622ea8b
Molina-Q <quentinmolina1@gmail.com>
2024-02-23 11:46:23 +0100

    Created SingleSectionInArticle component to show individual section in article

---
+++ assets/react/controllers/components/modal/SingleSectionInArticle.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SingleSectionInArticle({section, showDetails}) {
+  return (
+    // key so that React can diffentiate them
+    <div key={section.id} className='section-items' >
+
+        {/* the link created higher */}
+        <h3 onClick={() => {showDetails(section.id)}}>{section.title}</h3>
+
+        <p>{section.text}</p>
+
+    </div>
+  )
+}
+
+export default SingleSectionInArticle
