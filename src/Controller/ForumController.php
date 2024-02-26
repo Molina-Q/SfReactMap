8a90b8033c342d763506f440381c2da6239b1666
Molina-Q <quentinmolina1@gmail.com>
2024-02-26 10:44:28 +0100

    Added show_topic route

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TopicRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ForumController extends AbstractController
 {
     #[Route('/forum', name: 'app_forum')]
-    public function index(): Response
+    public function index(TopicRepository $topicRepository): Response
     {
+
+        $topics = $topicRepository->findBy([], ['title' => 'ASC']);
+
         return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
+            'topics' => $topics,
+        ]);
+    }
+
+    #[Route('/forum/topic/{id}', name: 'show_topic')]
+    public function showTopic(
+        int $id,
+        TopicRepository $topicRepository,
+    ): Response
+    {
+
+        $topic = $topicRepository->findOneById($id);
+
+        return $this->render('forum/show.html.twig', [
+            'topic' => $topic,
         ]);
     }
 }
+
