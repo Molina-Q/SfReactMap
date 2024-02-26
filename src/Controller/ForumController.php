12eb64ab782f81f9a3d4fb632649fb2be146ba96
Molina-Q <quentinmolina1@gmail.com>
2024-02-26 21:02:08 +0100

    Added formCreate

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Topic;
+use App\Form\TopicFormType;
 use App\Repository\TopicRepository;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,13 +21,40 @@ class ForumController extends AbstractController
         
     ): Response
     {
-
         $topics = $topicRepository->findBy([], ['title' => 'ASC']);
         $equipCateg = $categoryRepository->findAll();
 
         return $this->render('forum/index.html.twig', [
             'topics' => $topics,
             'equipCateg' => $equipCateg
+        ]);
+    }
+
+    #[Route('/forum/create', name: 'create_topic')]
+    public function create(
+        TopicRepository $topicRepository,
+        Request $request,
+        EntityManagerInterface $entityManager
+        
+    ): Response
+    {
+        $topic = new Topic();
+
+        $form = $this->createForm(TopicFormType::class, $topic, ['attr' => ['class' => 'form-create']]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($topic);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully created');
+
+            return $this->redirectToRoute('app_forum');
+        }
+
+        return $this->render('forum/create.html.twig', [
+            'createTopicForm' => $form->createView(),
         ]);
     }
 
