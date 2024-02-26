ebf1b8f08d67efbcb60f9171a178a185068fdc9e
Molina-Q <quentinmolina1@gmail.com>
2024-02-26 16:14:55 +0100

    Added topic/list route

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\TopicRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,15 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ForumController extends AbstractController
 {
     #[Route('/forum', name: 'app_forum')]
-    public function index(TopicRepository $topicRepository): Response
+    public function index(
+        TopicRepository $topicRepository,
+        CategoryRepository $categoryRepository
+        
+    ): Response
     {
 
         $topics = $topicRepository->findBy([], ['title' => 'ASC']);
+        $equipCateg = $categoryRepository->findAll();
 
         return $this->render('forum/index.html.twig', [
             'topics' => $topics,
+            'equipCateg' => $equipCateg
         ]);
     }
+
+
 
     #[Route('/forum/topic/{id}', name: 'show_topic')]
     public function showTopic(
@@ -32,6 +41,35 @@ class ForumController extends AbstractController
         return $this->render('forum/show.html.twig', [
             'topic' => $topic,
         ]);
+    }   
+
+    #[Route('/forum/topic/list/{sortBy}/{id?}', name: 'list_topic', defaults: ['id' => null])]
+    public function listTopics(
+        string $sortBy,
+        int $id = null,
+        TopicRepository $topicRepository,
+    ): Response
+    {
+        $topics;
+        switch ($sortBy) {
+            case 'equip':
+                $topics = $topicRepository->findByEquip($id);
+                break;
+            case 'article':
+                $topics = $topicRepository->findByArticle();
+                break;
+
+            default:
+                redirectToRoute('app_forum');
+                break;
+        }
+
+        // $topics = $topicRepository->findBy($id);
+
+        return $this->render('forum/list.html.twig', [
+            'topics' => $topics,
+        ]);
     }
+
 }
 
