244e7715df8e7dd64d7759d3fc3e857acea4ecad
Molina-Q <quentinmolina1@gmail.com>
2024-02-26 16:16:03 +0100

    Created FormType for create and update purpose

---
+++ src/Form/TopicFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Equipment;
+use App\Entity\Topic;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TopicFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('creationDate')
+            ->add('Author', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+            ->add('Equipment', EntityType::class, [
+                'class' => Equipment::class,
+'choice_label' => 'id',
+            ])
+            ->add('Article', EntityType::class, [
+                'class' => Article::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Topic::class,
+        ]);
+    }
+}
