69794cba9a2a328d1dd01536446e9f77b8e1105f
Molina-Q <quentinmolina1@gmail.com>
2024-02-27 17:04:51 +0100

    update FormType for create and update

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use Date;
+use DateTime;
 use App\Entity\Topic;
+use App\Entity\Message;
 use App\Form\TopicFormType;
+use App\Repository\UserRepository;
 use App\Repository\TopicRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -34,22 +38,76 @@ class ForumController extends AbstractController
     #[Route('/forum/create', name: 'create_topic')]
     public function create(
         TopicRepository $topicRepository,
+        UserRepository $userRepository,
         Request $request,
         EntityManagerInterface $entityManager
         
     ): Response
     {
         $topic = new Topic();
+        $messages = new Message();
+        $user = $userRepository->findOneById(1);
+        $form = $this->createForm(TopicFormType::class, $topic, ['attr' => ['class' => 'form-create']]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            // $message->setAuthor($topic->getAuthor()->getId());
+            $dateNow = new \DateTime('now');
+            $topic->setAuthor($user);
+            $topic->setCreationDate($dateNow);
+            
+            $entityManager->persist($topic);
+            $entityManager->flush();
+
+            $messages->setAuthor($user);
+            $messages->setTopic($topic);
+
+            // $message->setText($form->get('msgAuthor')->getData());
+
+            $messages->setCreationDate($dateNow);
+            $messages->setText($form->get('text')->getData());
+        
+
+            $entityManager->persist($messages);
+            $entityManager->flush();
+
+            $topic->addMessage($messages);
+
+            $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully created');
+
+            return $this->redirectToRoute('app_forum');
+        }
+// Got myself a nice green sword, but not very magic
+// I took it from a sleeping fat guy with horns.
+        return $this->render('forum/create.html.twig', [
+            'createTopicForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/forum/update/{id}', name: 'update_topic')]
+    public function update(
+        TopicRepository $topicRepository,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        int $id
+        
+    ): Response
+    {
+        $topic = $topicRepository->findOneById($id);
+        $message = new Message();
 
         $form = $this->createForm(TopicFormType::class, $topic, ['attr' => ['class' => 'form-create']]);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
 
+            $topic->setMsgAuthor($form->get('msgAuthor')->getData());
+
             $entityManager->persist($topic);
             $entityManager->flush();
 
-            $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully created');
+            $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
 
             return $this->redirectToRoute('app_forum');
         }
@@ -58,7 +116,6 @@ class ForumController extends AbstractController
             'createTopicForm' => $form->createView(),
         ]);
     }
-
 
 
     #[Route('/forum/topic/{id}', name: 'show_topic')]
