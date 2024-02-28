57093eb07244fe49b0bfee647fbc803d72f21232
Molina-Q <quentinmolina1@gmail.com>
2024-02-28 16:33:37 +0100

    Added toString

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -339,4 +339,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->username;
+    }
 }
