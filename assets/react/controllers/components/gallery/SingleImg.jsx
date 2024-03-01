2af2de268aaba4f7e7f0c93c9c037d7c477b7139
Molina-Q <quentinmolina1@gmail.com>
2024-03-01 16:41:22 +0100

    Changed props name

--- assets/react/controllers/components/gallery/SingleImg.jsx
+++ assets/react/controllers/components/gallery/SingleImg.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function SingleImg({item}) {
     // path to the picture of the item
-    const pathImg = `/img/upload/${item.Equipment.imgObjects[0].Img.path}`;
+    // const pathImg = `/img/upload/${item.Equipment.imgObjects[0].Img.path}`;
 
     return ( 
         <figure key={item.id} className='gallery-items' >
-            <img src={pathImg} alt={item.Equipment.name} />
+            <img src={12} alt={item.name} />
 
             <figcaption>
-                <a href='/forum'>{item.Equipment.name}</a>
+                <a href='/forum'>{item.name}</a>
             </figcaption>
         </figure>
     )
