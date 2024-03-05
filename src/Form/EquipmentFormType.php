3fd3ad4b48fa9af73637d524cd9504f6df909423
Molina-Q <quentinmolina1@gmail.com>
2024-03-05 20:07:23 +0100

    update equipFormType

--- src/Form/EquipmentFormType.php
+++ src/Form/EquipmentFormType.php
@@ -4,22 +4,37 @@ namespace App\Form;
 
 use App\Entity\Equipment;
 use App\Entity\SubCategory;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class EquipmentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('text')
+            ->add('name', TextType::class, [])
+            ->add('text', TextareaType::class, [])
+
             ->add('sub_category', EntityType::class, [
                 'class' => SubCategory::class,
-'choice_label' => 'id',
+                'choice_label' => 'subAndCat',
             ])
+
+            // ->add('sub_category', CollectionType::class, [
+            //       // each entry in the array will be an "email" field
+            //     'entry_type' => TextType::class,
+            //     // these options are passed to each "email" type
+            //     'entry_options' => [
+            //         'attr' => ['class' => 'email-box'],
+            //     ],
+            // ])
+
+            // ->add('picture')
         ;
     }
 
