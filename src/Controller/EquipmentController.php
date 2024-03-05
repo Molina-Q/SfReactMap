505b15769b525d73b5638b4e91bb3b8ff92a47e9
Molina-Q <quentinmolina1@gmail.com>
2024-03-05 20:06:14 +0100

    CreateEquip route

--- src/Controller/EquipmentController.php
+++ src/Controller/EquipmentController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipment;
+use App\Form\EquipmentFormType;
 use App\Repository\EquipmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +20,35 @@ class EquipmentController extends AbstractController
 
         return $this->render('equipment/index.html.twig', [
             'equipments' => $equipments,
+        ]);
+    }
+
+    #[Route('/equipment/create', name: 'create_equipment')]
+    public function create(
+        EquipmentRepository $equipmentRepository,
+        Request $request,
+        EntityManagerInterface $entityManager
+        ): Response
+    {
+        $equipment = new Equipment();
+
+        $user = $this->getUser(); // get the user currently logged in
+
+        $form = $this->createForm(EquipmentFormType::class, $equipment, ['attr' => ['class' => 'form-create']]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $dateNow = new \DateTime('now');
+
+            $entityManager->persist($equipment);
+            $entityManager->flush();
+
+            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully created');
+            return $this->redirectToRoute('app_equipment');
+        }
+
+        return $this->render('equipment/create.html.twig', [
+            'createEquipForm' => $form->createView(),
         ]);
     }
 }
