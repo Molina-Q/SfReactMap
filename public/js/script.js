6f92bf6d5616e7dfc51d412034e8a0f96fc61d98
Molina-Q <quentinmolina1@gmail.com>
2024-03-07 19:26:36 +0100

    changed var name e -> mouseEvent

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ const deleteBtn = document.getElementsByClassName("delete-btn");
 
 // open a confirm modal on click on a delete action
 for (const openDelete of deleteBtn) {
-    openDelete.addEventListener('click', (e) => (confirm("Do you really want to delete this ?") ? '' : e.preventDefault()));
+    openDelete.addEventListener('click', (mouseEvent) => (confirm("Do you really want to delete this ?") ? '' : mouseEvent.preventDefault()));
 }   
 
 // showCheckedCategory
