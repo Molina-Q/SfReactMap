ccb8ee3e985ca185c87c46dc8052e941c7b32d3d
Molina-Q <quentinmolina1@gmail.com>
2024-03-07 19:26:59 +0100

    added update & delete route

--- src/Controller/EquipmentController.php
+++ src/Controller/EquipmentController.php
@@ -93,4 +93,87 @@ class EquipmentController extends AbstractController
             'createEquipForm' => $form->createView(),
         ]);
     }
+
+    #[Route('/equipment/update/{id}', name: 'update_equipment')]
+    public function update(
+        EquipmentRepository $equipmentRepository,
+        ImgRepository $imgRepository,
+        ImgObjectRepository $imgObjectRepository,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        Int $id
+        ): Response
+    {
+        $equipment = $equipmentRepository->findOneById($id);
+        $img = new Img();
+        $imgObject = new ImgObject();
+
+        $user = $this->getUser(); // get the user currently logged in
+
+        $form = $this->createForm(EquipmentFormType::class, $equipment, ['attr' => ['class' => 'form-create']]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $dateNow = new \DateTime('now');
+
+            $entityManager->persist($equipment);
+            $entityManager->flush();
+
+            $uploadedFile = $form->get('path')->getData();
+
+            if ($uploadedFile) {
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                // $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $uploadedFile->move(
+                        'img/upload',
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    dd($e);
+                }
+
+                $img->setPath($newFilename);
+            }
+
+
+            // $entityManager->persist($img);
+            // $entityManager->flush();
+
+            // $imgObject->setEquipment($equipment);
+            // $imgObject->setImg($img);
+
+            // $entityManager->persist($imgObject);
+            // $entityManager->flush();
+
+
+            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully created');
+            return $this->redirectToRoute('app_equipment');
+        }
+
+        return $this->render('equipment/create.html.twig', [
+            'createEquipForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/equipment/delete/{id}', name: 'delete_equipment')]
+    public function delete(
+        EquipmentRepository $equipmentRepository,
+        EntityManagerInterface $entityManager,
+        Int $id
+        ): Response
+    {
+        $equipment = $equipmentRepository->findOneById($id);
+
+        if(isset($equipment)) {
+            $entityManager->remove($equipment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_equipment');
+    }
 }
