c3059e1741c694ed3a3be2220efae6bee0c09ef0
Molina-Q <quentinmolina1@gmail.com>
2024-03-09 21:43:48 +0100

    Added an edit btn

--- assets/react/controllers/components/modal/ShowArticle.jsx
+++ assets/react/controllers/components/modal/ShowArticle.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import SingleSectionInArticle from './SingleSectionInArticle'
+import LinkBtnTwoParams from '../UI/button/LinkBtnTwoParams'
 
 function ShowArticle({article, showDetails}) {
+  const idArticle = parseInt(article.id);
   return (
     <article>
         <h2>{article.Country.name} - {article.Century.year}</h2>
+        <LinkBtnTwoParams URI={`map/edit/article/${article.id}`}>
+          Edit
+        </LinkBtnTwoParams>
 
         <h1>{article.title}</h1> 
 
@@ -12,9 +17,9 @@ function ShowArticle({article, showDetails}) {
 
         <section className='section-container'>
           
-          {article.sections.map((section) => (
+          {article.sections.map((section) =>
             <SingleSectionInArticle key={section.id} section={section} showDetails={showDetails} />
-          ))}
+          )}
 
         </section>
 
