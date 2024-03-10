47e6f73c49d85e18550146fc8d57bfb160aa4c1b
Molina-Q <quentinmolina1@gmail.com>
2024-03-10 12:21:00 +0100

    Created FormType for Section

---
+++ src/Form/SectionFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Section;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SectionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('text')
+            ->add('summary')
+            ->add('Article', EntityType::class, [
+                'class' => Article::class,
+                'choice_label' => 'articleTag',
+                'disabled' => 'true'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Section::class,
+        ]);
+    }
+}
