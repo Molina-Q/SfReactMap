f8f1f5c5e841cf9ad8bd849c69cd43badd2fba7b
Molina-Q <quentinmolina1@gmail.com>
2024-03-10 19:22:16 +0100

    Updated editSection

--- src/Controller/SectionController.php
+++ src/Controller/SectionController.php
@@ -14,16 +14,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SectionController extends AbstractController
 {
-    #[Route('/section/create/{id}', name: 'create_section')]
+    #[Route('/section/create/{idArticle}', name: 'create_section')]
     public function index(
-        int $id,
+        int $idArticle,
         ArticleRepository $articleRepository,
         SectionRepository $sectionRepository,
         EntityManagerInterface $entityManager,
         Request $request
     ): Response
     {
-        $article = $articleRepository->findOneById($id);
+        $article = $articleRepository->findOneById($idArticle);
 
         $section = new Section();
         $section->setArticle($article);
@@ -48,11 +48,32 @@ class SectionController extends AbstractController
         ]);
     }
 
-    #[Route('/section/edit/{id}', name: 'edit_section')]
-    public function edit(): Response
+    #[Route('/section/edit/{idSection}', name: 'edit_section')]
+    public function edit(
+        ArticleRepository $articleRepository,
+        SectionRepository $sectionRepository,
+        EntityManagerInterface $entityManager,
+        Request $request,
+        int $idSection,
+    ): Response
     {
-        return $this->render('section/index.html.twig', [
-            'controller_name' => 'SectionController',
+        $section = $sectionRepository->findOneById($idSection);
+
+        $form = $this->createForm(SectionFormType::class, $section, ['attr' => ['class' => 'form-create']]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($section);
+            $entityManager->flush();
+
+            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
+
+            return $this->redirectToRoute('app_map');
+        }
+
+        return $this->render('map/createSection.html.twig', [
+            'createSectionForm' => $form->createView(),
         ]);
     }
 
