3d60bf055d65256f561a5e8b6e9d8b05441fb9f0
Molina-Q <quentinmolina1@gmail.com>
2024-03-10 12:19:56 +0100

    fixed typos

--- assets/react/controllers/components/modal/SingleSectionInArticle.jsx
+++ assets/react/controllers/components/modal/SingleSectionInArticle.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function SingleSectionInArticle({section, showDetails}) {
   return (
-    // key so that React can diffentiate them
+    // key so that React can differentiate them
     <div key={section.id} className='section-items' >
 
-        {/* the link created higher */}
-        <h3 onClick={() => {showDetails(section.id)}}>{section.title}</h3>
+      {/* the link created higher */}
+      <h3 onClick={() => {showDetails(section.id)}}>{section.title}</h3>
 
-        <p>{section.text}</p>
+      <p>{section.text}</p>
 
     </div>
   )
