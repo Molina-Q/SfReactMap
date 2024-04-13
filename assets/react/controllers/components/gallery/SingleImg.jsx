eac9b99575fa85f536c3f8d6067313f5175de3e4
Molina-Q <quentinmolina1@gmail.com>
2024-04-13 22:23:47 +0200

    changed alt

--- assets/react/controllers/components/gallery/SingleImg.jsx
+++ assets/react/controllers/components/gallery/SingleImg.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function SingleImg({item}) {
-    // path to the picture of the item
-    const pathImg = `/img/upload/${item.img}`;
+function SingleImg({ item }) {
+  // path to the picture of the item
+  const pathImg = `/img/upload/${item.img}`;
 
-    return ( 
-        <figure key={item.id} className='gallery-items' >
-            <img src={pathImg} alt={item.name} />
+  return (
+    <figure key={item.id} className={item.name}>
+      <img src={pathImg} alt={item.name} />
 
-            <figcaption>
-                <a href='/forum'>{item.name}</a>
-            </figcaption>
-        </figure>
-    )
+      <figcaption>
+        <a href="/forum">{item.name}</a>
+      </figcaption>
+    </figure>
+  );
 }
 
-export default SingleImg
+export default SingleImg;
