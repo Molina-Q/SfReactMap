c87776469b69533939261b5dd2413c8531d9c1b7
Molina-Q <quentinmolina1@gmail.com>
2024-04-13 22:17:01 +0200

    template of the file

--- assets/react/controllers/components/equipment/DetailsItem.jsx
+++ assets/react/controllers/components/equipment/DetailsItem.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function DetailsItem() {
-  return (
-    <div>DetailsItem</div>
-  )
+   return (
+      <aside class="equip-description">
+         <h2 id="equip-name">Click on an equipment to get it's details !</h2>
+         <figure>
+            <img src={image} alt="" />
+            <figcaption class="weapon-details">
+               <ul>
+                  <li>Sharp</li>
+                  <li>Blue?</li>
+                  <li>Glow in the dark</li>
+               </ul>
+            </figcaption>
+         </figure>
+
+         <div>
+            <p>
+              {text}
+            </p>
+         </div>
+      </aside>
+   );
 }
 
-export default DetailsItem
+export default DetailsItem;
