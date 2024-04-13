696724a398e6fb54494a7d891fa510eb0d00ec5c
Molina-Q <quentinmolina1@gmail.com>
2024-04-13 22:20:09 +0200

    made code more readable

--- assets/react/controllers/components/equipment/DetailsItem.jsx
+++ assets/react/controllers/components/equipment/DetailsItem.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 
-function DetailsItem() {
-   return (
-      <aside class="equip-description">
-         <h2 id="equip-name">Click on an equipment to get it's details !</h2>
-         <figure>
-            <img src={image} alt="" />
-            <figcaption class="weapon-details">
-               <ul>
-                  <li>Sharp</li>
-                  <li>Blue?</li>
-                  <li>Glow in the dark</li>
-               </ul>
-            </figcaption>
-         </figure>
+function DetailsItem(props) {
+  return (
+    <aside class="equip-description">
+      <h2 id="equip-name">{props.name}</h2>
+      <figure>
+        <img src={props.image} alt="" />
+        <figcaption class="weapon-details">
+          <ul>
+            <li>Sharp</li>
+            <li>Blue?</li>
+            <li>Glow in the dark</li>
+          </ul>
+        </figcaption>
+      </figure>
 
-         <div>
-            <p>
-              {text}
-            </p>
-         </div>
-      </aside>
-   );
+      <div>
+        <p>{props.text}</p>
+      </div>
+    </aside>
+  );
 }
 
 export default DetailsItem;
