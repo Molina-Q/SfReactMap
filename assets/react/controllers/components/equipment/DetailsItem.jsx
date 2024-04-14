4018f4dcf94ffa80d6eaaa8df9270cb51b580e25
Molina-Q <quentinmolina1@gmail.com>
2024-04-14 14:19:25 +0200

    removed weapon details and added ternary for title

--- assets/react/controllers/components/equipment/DetailsItem.jsx
+++ assets/react/controllers/components/equipment/DetailsItem.jsx
@@ -4,18 +4,11 @@ function DetailsItem(props) {
   const imgUrl = `/img/upload/${props.img}`;
   return (
     <>
-      <h2 id="equip-name">{props.name ?? 'Click on an item !'}</h2>
+      <h2 id="equip-name">{props.name ? props.name : 'Click on an item !'}</h2>
       {props.img && props.text && props.name && (
         <>
           <figure>
             <img src={imgUrl} alt={props.name} />
-            <figcaption className="weapon-details">
-              <ul>
-                <li>Sharp</li>
-                <li>Blue?</li>
-                <li>Glow in the dark</li>
-              </ul>
-            </figcaption>
           </figure>
 
           <div>
