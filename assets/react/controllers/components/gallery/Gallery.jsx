69f1fdefd113a310ad608412fae2a70cc034bca4
Molina-Q <quentinmolina1@gmail.com>
2024-04-14 11:59:37 +0200

    added classname and clickevent props

--- assets/react/controllers/components/gallery/Gallery.jsx
+++ assets/react/controllers/components/gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SingleImg from "./SingleImg";
 
-function Gallery({ items }) {
+function Gallery({ items, className, clickEvent }) {
   return (
     <>
       {items.map((item) => (
-        <SingleImg key={item.id} item={item} />
+        <SingleImg key={item.id} item={item} className={className} url={`/api/equipment/${item.id}`} clickEvent={clickEvent} />
       ))}
     </>
   );
