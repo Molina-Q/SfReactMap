27df64843fd9d06fd697508dfc2ca926e64fa0fb
Molina-Q <quentinmolina1@gmail.com>
2024-04-14 12:00:35 +0200

    minor changes

--- assets/react/controllers/components/modal/ShowSection.jsx
+++ assets/react/controllers/components/modal/ShowSection.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import Gallery from '../gallery/Gallery'
-import LinkBtn from '../UI/button/LinkBtn'
+import React from "react";
+import Gallery from "../gallery/Gallery";
+import LinkBtn from "../UI/button/LinkBtn";
 
-function ShowSection({section}) {
+function ShowSection({ section }) {
   return (
     <article>
-
-      <LinkBtn URI={`section/edit/${section.id}`} className={'delete-btn'}>
+      <LinkBtn URI={`section/edit/${section.id}`} className={"delete-btn"}>
         Edit
       </LinkBtn>
 
       <h1>{section.title}</h1>
 
-      <h2>{section.summary}</h2> 
+      <h2>{section.summary}</h2>
 
-      <p>{section.text}</p> 
+      <p>{section.text}</p>
 
-      <section className='section-gallery-container'>
-        {<Gallery items={section.Equipments} />}
+      <section className="section-gallery-container">
+        {<Gallery items={section.Equipments} className={"gallery-items"} />}
       </section>
-
-    </article> 
-  )
+    </article>
+  );
 }
 
-export default ShowSection
+export default ShowSection;
