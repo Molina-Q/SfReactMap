2681b2276e85e980bd534ddabfd766bf498ea286
Molina-Q <quentinmolina1@gmail.com>
2024-04-14 12:00:12 +0200

    added click event and classname props

--- assets/react/controllers/components/gallery/SingleImg.jsx
+++ assets/react/controllers/components/gallery/SingleImg.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-function SingleImg({ item }) {
+function SingleImg({ item, className, clickEvent }) {
   // path to the picture of the item
   const pathImg = `/img/upload/${item.img}`;
 
   return (
-    <figure key={item.id} className={item.name}>
+    <figure key={item.id} className={className} onClick={()=>clickEvent(item.id)}>
       <img src={pathImg} alt={item.name} />
 
       <figcaption>
-        <a href="/forum">{item.name}</a>
+        <a href={ item.url ? item.url : "/forum"}>{item.name}</a>
       </figcaption>
     </figure>
   );
