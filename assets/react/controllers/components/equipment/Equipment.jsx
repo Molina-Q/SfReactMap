6ae4300f513a348ae65330c39eef8dc21c9746d1
Molina-Q <quentinmolina1@gmail.com>
2024-04-14 14:20:13 +0200

    added useState for selected categories and useEffect to change what is shown

--- assets/react/controllers/components/equipment/Equipment.jsx
+++ assets/react/controllers/components/equipment/Equipment.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { fetchAnything } from "../../../../tools/Fetchs";
 import Gallery from "../gallery/Gallery";
 import Loading from "../UI/animation/Loading";
@@ -10,14 +10,20 @@ export default function Equipment() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // category data
+  const [categories, setCategories] = useState({});
+
   // states for the details page data
   const [clickedItem, setClickedItem] = useState({});
   const [loadingDetails, setLoadingDetails] = useState(false);
 
+  // state for the current selected category
+  const [categoryType, setCategoryType] = useState(1);
+
   // trigger onClick of an img from the gallery
   const clickEvent = (id) => {
     setLoadingDetails(true);
-    fetchData(`/equipment/api/${id}`, setClickedItem, setLoadingDetails);
+    fetchData(`/api/equipment/${id}`, setClickedItem, setLoadingDetails);
   };
 
   async function fetchData(URI, dataState, loadingState) {
@@ -27,7 +33,7 @@ export default function Equipment() {
 
     if (dataFetch) {
       // set the content of given state data
-      loadingState(false);
+      loadingState ? loadingState(false) : "";
       dataState(dataFetch);
     } else {
       // there was a mistake and the data is null
@@ -35,38 +41,83 @@ export default function Equipment() {
     }
   }
 
+  // test purpose useEffect
   useEffect(() => {
     if (clickedItem.equipment) {
       console.log("data clicked item = ", clickedItem.equipment.id);
     }
   });
 
+  // call fetchData when page is loading then when category type is updated
   useEffect(() => {
-    fetchData("/equipment/api", setData, setLoading);
+    fetchData(`/api/equipment/type/${categoryType}`, setData, setLoading);
+  }, [categoryType]); // will re-run only when one of those variables changes (using Object.js comparison)
+
+  useLayoutEffect(() => {
+    fetchData(`/api/equipment/types`, setCategories);
   }, []); // will re-run only when one of those variables changes (using Object.js comparison)
 
+  const handleClick = (e) => {
+    if (
+      e.target.getAttribute("data-id") == 1 ||
+      e.target.getAttribute("data-id") == 2 ||
+      e.target.getAttribute("data-id") == 3
+    ) {
+      setCategoryType(e.target.getAttribute("data-id"));
+    }
+  };
+
+  if(categories.categories) {
+    console.log('data type = ',categories.categories);
+  }
+
   return (
-    <article className="equip-menu">
-      <article className="equip-gallery">
-        {loading ? (
-          <Loading />
-        ) : (
-          <Gallery
-            items={data["equipment"]}
-            className={"equip-items"}
-            clickEvent={clickEvent}
-          />
-        )}
+    <>
+      {categories.categories &&
+        categories.categories.map((catType) => (
+          <strong
+            key={catType.id}
+            data-id={catType.id}
+            onClick={handleClick}
+            className={"cat-" + catType.label.toLowerCase()}
+          >
+            {catType.label}
+          </strong>
+        ))}
+      {/* <strong onClick={handleClick} className="cat-weapon">
+        Weapon
+      </strong>
+      <strong onClick={handleClick} className="cat-armour">
+        Armour
+      </strong>
+      <strong onClick={handleClick} className="cat-tool">
+        Tool
+      </strong> */}
+
+      <article className="equip-menu">
+        <article className="equip-gallery">
+          {loading ? (
+            <Loading />
+          ) : (
+            <Gallery
+              items={data["equipments"]}
+              className={"equip-items"}
+              clickEvent={clickEvent}
+            />
+          )}
+        </article>
+        <aside className="equip-description">
+          {clickedItem.equipment ? (
+            <DetailsItem
+              name={clickedItem.equipment.name}
+              img={clickedItem.equipment.img}
+              text={clickedItem.equipment.text}
+            />
+          ) : (
+            <DetailsItem />
+          )}
+        </aside>
       </article>
-      <aside className="equip-description">
-        {clickedItem.equipment && (
-          <DetailsItem
-            name={clickedItem.equipment.name}
-            img={clickedItem.equipment.img}
-            text={clickedItem.equipment.text}
-          />
-        )}
-      </aside>
-    </article>
+    </>
   );
 }
