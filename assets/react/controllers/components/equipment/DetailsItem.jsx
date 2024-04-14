0562f038e0e3c8f4f302d87e1b6bb53a8f4f91e8
Molina-Q <quentinmolina1@gmail.com>
2024-04-14 11:58:36 +0200

    added imgUrl variable and updated html elements

--- assets/react/controllers/components/equipment/DetailsItem.jsx
+++ assets/react/controllers/components/equipment/DetailsItem.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 
 function DetailsItem(props) {
+  const imgUrl = `/img/upload/${props.img}`;
   return (
-    <aside class="equip-description">
-      <h2 id="equip-name">{props.name}</h2>
-      <figure>
-        <img src={props.image} alt="" />
-        <figcaption class="weapon-details">
-          <ul>
-            <li>Sharp</li>
-            <li>Blue?</li>
-            <li>Glow in the dark</li>
-          </ul>
-        </figcaption>
-      </figure>
+    <>
+      <h2 id="equip-name">{props.name ?? 'Click on an item !'}</h2>
+      {props.img && props.text && props.name && (
+        <>
+          <figure>
+            <img src={imgUrl} alt={props.name} />
+            <figcaption className="weapon-details">
+              <ul>
+                <li>Sharp</li>
+                <li>Blue?</li>
+                <li>Glow in the dark</li>
+              </ul>
+            </figcaption>
+          </figure>
 
-      <div>
-        <p>{props.text}</p>
-      </div>
-    </aside>
+          <div>
+            <p>{props.text}</p>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
