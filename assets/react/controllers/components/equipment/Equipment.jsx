f72c8e6108e0699284fbbcb77b4a5bbe4ec6db79
Molina-Q <quentinmolina1@gmail.com>
2024-04-17 21:31:49 +0200

    re organized and now the url change to reflect the change in the pages

--- assets/react/controllers/components/equipment/Equipment.jsx
+++ assets/react/controllers/components/equipment/Equipment.jsx
@@ -1,118 +1,139 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchAnything } from "../../../../tools/Fetchs";
 import Gallery from "../gallery/Gallery";
 import Loading from "../UI/animation/Loading";
-import EquipItems from "./EquipItems";
 import DetailsItem from "./DetailsItem";
+import { setUrlParam, getUrlParam } from "../../../../tools/UrlParam";
 
 export default function Equipment() {
-  // states for the gallery data
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+	// states for the gallery data
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-  // category data
-  const [categories, setCategories] = useState({});
+	// category data
+	const [categories, setCategories] = useState({});
 
-  // states for the details page data
-  const [clickedItem, setClickedItem] = useState({});
-  const [loadingDetails, setLoadingDetails] = useState(false);
+  // current active category type
+  const [currentCategory, setCurrentCategory] = useState(null)
 
-  // state for the current selected category
-  const [categoryType, setCategoryType] = useState(1);
+	// states for the details page data
+	const [clickedItemData, setClickedItemData] = useState({});
+	const [loadingDetails, setLoadingDetails] = useState(false);
 
-  // trigger onClick of an img from the gallery
-  const clickEvent = (id) => {
-    setLoadingDetails(true);
-    fetchData(`/api/equipment/${id}`, setClickedItem, setLoadingDetails);
-  };
+	const [urlData, setUrlData] = useState({
+		type: getUrlParam("type") || 1,
+		item: getUrlParam("item") || "",
+	});
 
-  async function fetchData(URI, dataState, loadingState) {
-    console.log("-- Fetch - called --");
-    const dataFetch = await fetchAnything(URI);
-    console.log("-- Fetch - received --");
+	// onClick function for the images of the gallery
+	const handleClickImage = (id) => {
+		setLoadingDetails(true);
+		setUrlParam("item", id);
+		setUrlData({ ...urlData, item: id });
+	};
 
-    if (dataFetch) {
-      // set the content of given state data
-      loadingState ? loadingState(false) : "";
-      dataState(dataFetch);
-    } else {
-      // there was a mistake and the data is null
-      console.error("MANUAL ERRROR: Invalid data");
-    }
-  }
+  // onClick function for the categories
+  const handleClickCategory = (e) => {
+		if (
+			e.target.getAttribute("data-id") == 1 ||
+			e.target.getAttribute("data-id") == 2 ||
+			e.target.getAttribute("data-id") == 3
+		) {
+			setUrlParam("type", e.target.getAttribute("data-id"));
 
-  // test purpose useEffect
-  useEffect(() => {
-    if (clickedItem.equipment) {
-      console.log("data clicked item = ", clickedItem.equipment.id);
-    }
-  });
+			setUrlData({ ...urlData, type: e.target.getAttribute("data-id") });
 
-  // call fetchData when page is loading then when category type is updated
-  useEffect(() => {
-    fetchData(`/api/equipment/type/${categoryType}`, setData, setLoading);
-  }, [categoryType]); // will re-run only when one of those variables changes (using Object.js comparison)
+      setCurrentCategory(e.target.getAttribute("data-id"));
 
-  useEffect(() => {
-    fetchData(`/api/equipment/types`, setCategories);
-  }, []); // will re-run only when one of those variables changes (using Object.js comparison)
+			setLoading(true);
+		}
+	};
 
-  const handleClick = (e) => {
-    if (
-      e.target.getAttribute("data-id") == 1 ||
-      e.target.getAttribute("data-id") == 2 ||
-      e.target.getAttribute("data-id") == 3
-    ) {
-      setCategoryType(e.target.getAttribute("data-id"));
-      setLoading(true);
-    }
-  };
+	async function fetchData(URI, dataState, loadingState) {
+		console.log("-- Fetch - called --");
 
-  if (data) {
-    console.log("data type = ", data["equipments"].length);
-  }
+		const dataFetch = await fetchAnything(URI);
 
-  return (
-    <>
-      {categories.categories &&
-        categories.categories.map((catType) => (
-          <strong
-            key={catType.id}
-            data-id={catType.id}
-            onClick={handleClick}
-            className={"cat-" + catType.label.toLowerCase()}
-          >
-            {catType.label}
-          </strong>
-        ))}
+		console.log("-- Fetch - received --");
 
-      <article className="equip-menu">
-        <article className="equip-gallery">
-          {loading ? (
-            <Loading />
-          ) : data["equipments"].length === 0 ? (
-            <h2>There is no items here yet sorry for the inconvenience !</h2>
-          ) : (
-            <Gallery
-              items={data["equipments"]}
-              className={"equip-items"}
-              clickEvent={clickEvent}
-            />
-          )}
-        </article>
+		if (dataFetch) {
+			// set the content of given state data
+			loadingState ? loadingState(false) : "";
+			dataState(dataFetch);
+		} else {
+			// there was a mistake and the data is null
+			console.error("MANUAL ERRROR: Invalid data");
+		}
+	}
 
-        <aside className="equip-description">
-          {clickedItem.equipment ? (
-            <DetailsItem
-              name={clickedItem.equipment.name}
-              img={clickedItem.equipment.img}
-              text={clickedItem.equipment.text}
-            />
-          ) : (
-            <DetailsItem />
-          )}
-        </aside>
-      </article>
-    </>
-  );
+  	// call fetchData when page is loading then when category type is updated
+	useEffect(() => {
+    // if ((!currentCategory) || (currentCategory)) {
+      fetchData(`/api/equipment/type/${urlData.type}`, setData, setLoading);
+    // }
+
+		if ((urlData.item && !clickedItemData.equipment) || (clickedItemData.equipment && urlData.item != clickedItemData.equipment.id) ) {
+			setLoadingDetails(true);
+			fetchData(
+				`/api/equipment/${urlData.item}`,
+				setClickedItemData,
+				setLoadingDetails
+			);
+		}
+	}, [urlData]); // will re-run only when one of those variables changes (using Object.js comparison)
+
+	useEffect(() => {
+		fetchData(`/api/equipment/types`, setCategories);
+	}, []); // will re-run only when one of those variables changes (using Object.js comparison)
+
+
+	if (data) {
+		console.log("data type = ", data["equipments"].length);
+	}
+
+	return (
+		<>
+			{categories.categories &&
+				categories.categories.map((catType) => (
+					<strong
+						key={catType.id}
+						data-id={catType.id}
+						onClick={handleClickCategory}
+						className={"cat-" + catType.label.toLowerCase()}
+					>
+						{catType.label}
+					</strong>
+				))}
+
+			<article className="equip-menu">
+				<article className="equip-gallery">
+					{loading ? (
+						<Loading />
+					) : data["equipments"].length === 0 ? (
+						<h2>There is no items here yet sorry for the inconvenience !</h2>
+					) : (
+						<Gallery
+							items={data["equipments"]}
+							className={"equip-items"}
+							clickEvent={handleClickImage}
+						/>
+					)}
+				</article>
+
+				<aside className="equip-description">
+					{loadingDetails ? (
+						<Loading />
+					) : clickedItemData.equipment ? (
+						<DetailsItem
+							name={clickedItemData.equipment.name}
+							img={clickedItemData.equipment.img}
+							text={clickedItemData.equipment.text}
+						/>
+					) : (
+						<DetailsItem />
+					)}
+				</aside>
+			</article>
+		</>
+	);
 }
