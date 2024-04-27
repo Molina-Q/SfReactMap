e423aca883ac9a480f4da9d47cf2204be1f6d0db
Molina-Q <quentinmolina1@gmail.com>
2024-04-27 22:42:58 +0200

    prepared to add navbar

--- assets/App.jsx
+++ assets/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import SfReactMap from "./pages/SfReactMap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Equipment from "./pages/Equipment";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
+    {/* <Navbar /> */}
       <Routes>
         <Route path="/map" element={<SfReactMap />} />
         <Route path="/equipment" element={<Equipment />} />
