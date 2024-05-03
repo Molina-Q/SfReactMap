9ba4106e7c54623ff60f63e92a9a86c14b6c2a6a
Molina-Q <quentinmolina1@gmail.com>
2024-05-03 18:25:23 +0200

    Add proxy configuration for API requests

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import symfonyPlugin from "vite-plugin-symfony";
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        proxy: {
+          '/api': {
+            target: 'http://127.0.0.1:8000',
+            secure: false,
+          },
+        },
+      },
     plugins: [
         react(),
         symfonyPlugin(),
