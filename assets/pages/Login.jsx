38a4b4e375dcb4a719562237e5e2a7d350cceb6c
Molina-Q <quentinmolina1@gmail.com>
2024-05-03 18:28:48 +0200

    refactor: Update Login component to use loginData state instead of formData and added fetchLogin to get the csrf token from back-end

--- assets/pages/Login.jsx
+++ assets/pages/Login.jsx
@@ -1,35 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Alert from "../components/UI/Alert";
 import Loading from "../components/UI/animation/Loading";
+import { fetchAnything } from "../utils/Fetchs";
 
-export default function SignIn() {
-
-    const [formData, setFormData] = useState({ props });
+export default function Login() {
 	const navigate = useNavigate();
 	// const dispatch = useDispatch();
 	// const { loading, error: errorMessage } = useSelector((state) => state.user);
-    const [loading, setLoading] = useState(false);
-    const [errorMessage, setErrorMessage] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [loginData, setLoginData] = useState({ email: "", password: "", _csrf_token: ""});
+
 
 	const handleChange = (e) => {
-		setFormData({
-			...formData,
+		setLoginData({
+			...loginData,
 			[e.target.id]: e.target.value.trim(),
 		});
 	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (!formData.email || !formData.password) {
-			// return dispatch(signInFailure("Please fill out all fields!"));
+		if (!loginData.email || !loginData.password) {
+			return setErrorMessage("Please fill out all fields!");
 		}
 		try {
-			dispatch(signInStart());
-			const res = await fetch("/api/auth/signin", {
+			// dispatch(signInStart());
+			console.log(JSON.stringify({...loginData}));
+			const res = await fetch("/api/login", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(formData),
+				body: JSON.stringify({...loginData}),
 			});
 			const data = await res.json();
 			if (data.success === false) {
@@ -37,97 +39,79 @@ export default function SignIn() {
 			}
 			if (res.ok) {
 				// dispatch(signInSuccess(data));
-				navigate("/");
+				navigate("/home");
 			}
 		} catch (error) {
+			return setErrorMessage(error.message);
 			// dispatch(signInFailure(error));
 		}
 	};
 
+	useEffect(() => {
+		async function fetchLogin() {
+			// setLoading(true);
+
+			console.log("-- Fetch - called --");
+
+			const data = await fetchAnything("/api/login/show");
+
+			console.log("-- Fetch - received --");
+
+			// if the given data were valid
+			if (data) {
+				// set the content of data
+				setLoginData({ ...loginData, _csrf_token: data._csrf_token });
+			} else {
+				// there was a mistake and the data is null
+				// setLoading(false); // loading is set to false to avoid infinite loading
+				console.error("MANUAL ERRROR: Invalid data or Empty");
+				return;
+			}
+		}
+
+		fetchLogin();
+	}, []);
+
 	return (
+		<div className="login">
+			<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
 
-		
-			<div className="login">
-				<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-	
-				<form method="post" className="form-create">
-					{props.error && (
-						<div className="alert alert-danger">{props.error.messageKey}</div>
-					)}
-	
-					{props.user && (
-						<div className="mb-3">
-							You are logged in as {props.user.userIdentifier}, <a href={props.logoutPath}>Logout</a>
-						</div>
-					)}
-	
-					<label htmlFor="inputEmail">Email</label>
-					<input type="email" value={props.lastUsername} name="email" id="inputEmail" className="form-control" autoComplete="email" required />
-					<label htmlFor="inputPassword">Password</label>
-					<input type="password" name="password" id="inputPassword" className="form-control" autoComplete="current-password" required />
-	
-					<input type="hidden" name="_csrf_token" value={props.csrfToken} />
-	
-					<button className="form-btn-submit" type="submit">
-						Sign in
-					</button>
-				</form>
-			</div>
-		
-		// <div className="min-h-screen mt-20">
-		// 	<div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-		// 		{/* right side */}
-		// 		<div className="flex-1">
-		// 			<form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-		// 				<div>
-		// 					<label htmlFor="email" value="Your email" />
-		// 					<input
-		// 						type="email"
-		// 						placeholder="name@company.com"
-		// 						id="email"
-		// 						onChange={handleChange}
-		// 					/>
-		// 				</div>
-		// 				<div>
-		// 					<label htmlFor="password" value="Your password" />
-		// 					<input
-		// 						type="password"
-		// 						placeholder="Password"
-		// 						id="password"
-		// 						onChange={handleChange}
-		// 					/>
-		// 				</div>
-		// 				<button
-		// 					gradientDuoTone="purpleToPink"
-		// 					type="submit"
-		// 					disabled={loading}
-		// 				>
-		// 					{loading ? (
-		// 						<>
-		// 							<Loading />
-		// 							<span className="pl-2">Loading...</span>
-		// 						</>
-		// 					) : (
-		// 						"Sign In"
-		// 					)}
-		// 				</button>
-		// 				{/* <OAuth /> */}
-		// 			</form>
-		// 			<div className="flex gap-2 text-sm mt-5">
-		// 				<span>Dont have an account ?</span>
-		// 				<Link to="/register" className="text-blue-500">
-		// 					Sign Up
-		// 				</Link>
-		// 			</div>
-		// 			{errorMessage && (
-		// 				<Alert className="" color="failure">
-		// 					{errorMessage}
-		// 				</Alert>
-		// 			)}
-		// 		</div>
-		// 	</div>
-		// </div>
+			<form method="post" className="form-create">
+				{errorMessage && (
+					<div className="alert alert-danger">{errorMessage}</div>
+				)}
+
+				<label htmlFor="inputEmail">Email</label>
+				<input
+					type="email"
+					value={loginData.email}
+					name="email"
+					id="email"
+					className="form-control"
+					onChange={handleChange}
+					required
+				/>
+				<label htmlFor="inputPassword">Password</label>
+				<input
+					type="password"
+					value={loginData.password}
+					name="password"
+					id="password"
+					className="form-control"
+					onChange={handleChange}
+					required
+				/>
+
+				<input type="hidden" name="_csrf_token" value={loginData._csrf_token} />
+
+				<button
+					className="form-btn-submit"
+					type="submit"
+					onClick={handleSubmit}
+				>
+					Sign in
+				</button>
+			</form>
+		</div>
 	);
-  
 }
-
