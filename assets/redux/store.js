e365978c77eed818a3411f87528c3f8944a2ef1f
Molina-Q <quentinmolina1@gmail.com>
2024-05-04 20:16:24 +0200

    refactor: Update store.js to use userReducer directly in persistReducer

--- assets/redux/store.js
+++ assets/redux/store.js
@@ -3,15 +3,13 @@ import userReducer from "./user/userSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const rootReducer = combineReducers({ user: userReducer });
-
 const persistConfig = {
 	key: "root",
 	storage,
 	version: 1,
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
 	reducer: persistedReducer,
