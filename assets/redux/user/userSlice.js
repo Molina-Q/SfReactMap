040855c020ccca47e42e234d118609c1f0fe7081
Molina-Q <quentinmolina1@gmail.com>
2024-05-04 20:16:29 +0200

    refactor: Update userSlice to handle user sign in and sign out actions
    - Add setUser and clearUser reducers to update the currentUser state
    - Update initialState to set currentUser to null
    - Remove unused signInStart, signInSuccess, and signInFailure reducers

--- assets/redux/user/userSlice.js
+++ assets/redux/user/userSlice.js
@@ -1,35 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-	// initial value
+const initialState = { // initial value
 	currentUser: null,
-	error: null,
-	loading: false,
-};
+  };
 
 const userSlice = createSlice({
-	name: "user", // used this to call the slice in the components
-	initialState,
-	reducers: {
-		signInStart: (state) => {
-			state.loading = true;
-		},
-		signInSuccess: (state, action) => {
-			state.currentUser = action.payload; // data of the logged user
-			state.loading = false;
-			state.error = null;
-		},
-		signInFailure: (state, action) => {
-			state.error = action.payload;
-			state.loading = false;
-		},
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+		state.currentUser = action.payload;
 	},
+    clearUser: (state) => {
+		state.currentUser = null;
+	},
+  },
 });
 
-export const {
-	signInFailure,
-	signInStart,
-	signInSuccess,
-} = userSlice.actions;
+export const { setUser, clearUser } = userSlice.actions;
 
 export default userSlice.reducer;
