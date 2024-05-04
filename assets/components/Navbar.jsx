a7ca84bb1fd0718ab7ff0bb5df5dffea3ce41e96
Molina-Q <quentinmolina1@gmail.com>
2024-05-04 20:15:43 +0200

    refactor: Update Navbar component to include user sign out functionality

--- assets/components/Navbar.jsx
+++ assets/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { getUserSession } from "../utils/getUserSession";
+import { clearUser, setUser } from "../redux/user/userSlice";
 
 export default function Navbar() {
-	const { currentUser } = useSelector((state) => state.user);
+	const currentUser = useSelector((state) => state.user);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	console.log("User:", currentUser);
+	const handleSignOut = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await fetch("/logout");
+			const data = await res.json();
+			dispatch(clearUser());
+			navigate("/home");
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
 	return (
 		<header>
@@ -39,60 +55,57 @@ export default function Navbar() {
 
 				<div>
 					<div className="nav-items nav-right">
-						<a href="/home">
+						<Link to="/home">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
 								<path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H288V352c0-17.7 14.3-32 32-32h80V96c0-8.8-7.2-16-16-16H64zM288 480H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V320v5.5c0 17-6.7 33.3-18.7 45.3l-90.5 90.5c-12 12-28.3 18.7-45.3 18.7H288z" />
 							</svg>
 							About us
-						</a>
+						</Link>
 					</div>
 
-					{/* if connected */}
+					{/* if connected show this */}
 					{currentUser ? (
 						<>
 							<div className="nav-items nav-right">
-								<a href="{{ path('app_user') }}">
+								<Link to="/profile">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 										<path d="M224 0a128 128 0 1 1 0 256A128 128 0 1 1 224 0zM178.3 304h91.4c11.8 0 23.4 1.2 34.5 3.3c-2.1 18.5 7.4 35.6 21.8 44.8c-16.6 10.6-26.7 31.6-20 53.3c4 12.9 9.4 25.5 16.4 37.6s15.2 23.1 24.4 33c15.7 16.9 39.6 18.4 57.2 8.7v.9c0 9.2 2.7 18.5 7.9 26.3H29.7C13.3 512 0 498.7 0 482.3C0 383.8 79.8 304 178.3 304zM436 218.2c0-7 4.5-13.3 11.3-14.8c10.5-2.4 21.5-3.7 32.7-3.7s22.2 1.3 32.7 3.7c6.8 1.5 11.3 7.8 11.3 14.8v17.7c0 7.8 4.8 14.8 11.6 18.7c6.8 3.9 15.1 4.5 21.8 .6l13.8-7.9c6.1-3.5 13.7-2.7 18.5 2.4c7.6 8.1 14.3 17.2 20.1 27.2s10.3 20.4 13.5 31c2.1 6.7-1.1 13.7-7.2 17.2l-14.4 8.3c-6.5 3.7-10 10.9-10 18.4s3.5 14.7 10 18.4l14.4 8.3c6.1 3.5 9.2 10.5 7.2 17.2c-3.3 10.6-7.8 21-13.5 31s-12.5 19.1-20.1 27.2c-4.8 5.1-12.5 5.9-18.5 2.4l-13.8-7.9c-6.7-3.9-15.1-3.3-21.8 .6c-6.8 3.9-11.6 10.9-11.6 18.7v17.7c0 7-4.5 13.3-11.3 14.8c-10.5 2.4-21.5 3.7-32.7 3.7s-22.2-1.3-32.7-3.7c-6.8-1.5-11.3-7.8-11.3-14.8V467.8c0-7.9-4.9-14.9-11.7-18.9c-6.8-3.9-15.2-4.5-22-.6l-13.5 7.8c-6.1 3.5-13.7 2.7-18.5-2.4c-7.6-8.1-14.3-17.2-20.1-27.2s-10.3-20.4-13.5-31c-2.1-6.7 1.1-13.7 7.2-17.2l14-8.1c6.5-3.8 10.1-11.1 10.1-18.6s-3.5-14.8-10.1-18.6l-14-8.1c-6.1-3.5-9.2-10.5-7.2-17.2c3.3-10.6 7.7-21 13.5-31s12.5-19.1 20.1-27.2c4.8-5.1 12.4-5.9 18.5-2.4l13.6 7.8c6.8 3.9 15.2 3.3 22-.6c6.9-3.9 11.7-11 11.7-18.9V218.2zm92.1 133.5a48.1 48.1 0 1 0 -96.1 0 48.1 48.1 0 1 0 96.1 0z" />
 									</svg>
 									Profile
-								</a>
+								</Link>
 							</div>
 
 							<div className="nav-items nav-right">
-								<a href="/sign-out">
+								<Link to="#" onClick={handleSignOut}>
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 										<path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM471 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
 									</svg>
 									Logout
-								</a>
+								</Link>
 							</div>
 						</>
 					) : (
 						<>
+							{/* if not connected show this */}
 							<div className="nav-items nav-right">
-								<a href="/register">
+								<Link to="/register">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 										<path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H322.8c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1H178.3zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z" />
 									</svg>
 									Register
-								</a>
+								</Link>
 							</div>
 
 							<div className="nav-items nav-right">
-								<a href="/login">
+								<Link to="/login">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 										<path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM625 177L497 305c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L591 143c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
 									</svg>
 									Login
-								</a>
+								</Link>
 							</div>
 						</>
 					)}
-
-					{/* else not connected */}
-
-					{/* end */}
 				</div>
 			</nav>
 		</header>
