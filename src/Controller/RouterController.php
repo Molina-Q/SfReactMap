c01e3e69d8772522f6550d4974bb5a92afd176fc
Molina-Q <quentinmolina1@gmail.com>
2024-05-04 20:18:15 +0200

    refactor: Remove unused routes and templates in RouterController

--- src/Controller/RouterController.php
+++ src/Controller/RouterController.php
@@ -9,43 +9,23 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RouterController extends AbstractController
 {
-    // #[Route('/home', name: 'app_home', methods: ['GET'])]
-    // public function indexHome(): Response
-    // {
-    //     return $this->render('base.html.twig');
-    // }
-
-    // #[Route('/equipment', name: 'app_equipment', methods: ['GET'])]
-    // public function indexEquipment(): Response
-    // {
-    //     return $this->render('base.html.twig');
-    // }
-
-    // #[Route('/map', name: 'app_map', methods: ['GET'])]
-    // public function indexMap(): Response
-    // {
-    //     return $this->render('base.html.twig');
-    // }
-
-    // #[Route('/login', name: 'app_login', methods: ['GET'])]
-    // public function loginPage(): Response
-    // {    
-    //     return $this->render('base.html.twig');
-    // }
-
-    // #[Route('/register', name: 'app_show_register', methods: ['GET'])]
-    // public function showRegister(): Response
-    // {
-    //     return $this->render('map/index.html.twig');
-    // }
     #[Route('/home', name: 'app_home', methods: ['GET'])]
     #[Route('/equipment', name: 'app_equipment', methods: ['GET'])]
     #[Route('/map', name: 'app_map', methods: ['GET'])]
-    #[Route('/login', name: 'app_login', methods: ['GET'])]
-    #[Route('/register', name: 'app_show_register', methods: ['GET'])]
     #[Route('/forum', name: 'app_forum', methods: ['GET'])]
     public function index(): Response
     {
+        return $this->render('base.html.twig');
+    }
+
+    #[Route('/login', name: 'app_show_login', methods: ['GET'])]
+    #[Route('/register', name: 'app_show_register', methods: ['GET'])]
+    public function indexAuth(): Response
+    {
+        if($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('base.html.twig');
     }
 
