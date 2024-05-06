db281c4b7885ee7627976bf3cedefa13b6f8d0fa
Molina-Q <quentinmolina1@gmail.com>
2024-05-06 17:53:07 +0200

    chore: Refactor EquipmentController create method

--- src/Controller/EquipmentController.php
+++ src/Controller/EquipmentController.php
@@ -20,73 +20,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 class EquipmentController extends AbstractController
 {
 
-
-    #[Route('/equipment/create', name: 'create_equipment')]
-    public function create(
-        EquipmentRepository $equipmentRepository,
-        ImgRepository $imgRepository,
-        ImgObjectRepository $imgObjectRepository,
-        Request $request,
-        EntityManagerInterface $entityManager
-        ): Response
-    {
-        $equipment = new Equipment();
-        $img = new Img();
-        $imgObject = new ImgObject();
-
-        $user = $this->getUser(); // get the user currently logged in
-
-        $form = $this->createForm(EquipmentFormType::class, $equipment, ['attr' => ['class' => 'form-create']]);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()) {
-            $dateNow = new \DateTime('now');
-
-
-
-            $entityManager->persist($equipment);
-            $entityManager->flush();
-
-            $uploadedFile = $form->get('path')->getData();
-
-            if ($uploadedFile) {
-                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                // $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $originalFilename.'-'.uniqid().'.'.$uploadedFile->guessExtension();
-
-                // Move the file to the directory where brochures are stored
-                try {
-                    $uploadedFile->move(
-                        'img/upload',
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    dd($e);
-                }
-
-                $img->setPath($newFilename);
-            }
-
-
-            $entityManager->persist($img);
-            $entityManager->flush();
-
-            $imgObject->setEquipment($equipment);
-            $imgObject->setImg($img);
-
-            $entityManager->persist($imgObject);
-            $entityManager->flush();
-
-
-            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully created');
-            return $this->redirectToRoute('app_equipment');
-        }
-
-        return $this->render('equipment/create.html.twig', [
-            'createEquipForm' => $form->createView(),
-        ]);
-    }
+   
 
     #[Route('/equipment/update/{id}', name: 'update_equipment')]
     public function update(
