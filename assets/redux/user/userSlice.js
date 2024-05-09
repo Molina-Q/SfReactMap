81a2ba4587c03822ece8a6b58ae87a675efe86bc
Molina-Q <quentinmolina1@gmail.com>
2024-05-09 21:16:18 +0200

    refactor: Update userSlice to handle login success and error

--- assets/redux/user/userSlice.js
+++ assets/redux/user/userSlice.js
@@ -1,22 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { // initial value
-	currentUser: null,
-  };
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+
 
 const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    setUser: (state, action) => {
-		state.currentUser = action.payload;
+	name: "user",
+	initialState,
+	reducers: {
+		loginSuccess: (state, action) => {
+			state.currentUser = action.payload;
+			state.loading = false;
+			state.error = null;
+		},
+		loginError: (state,) => {
+			state.loading = false;
+			state.error = true;
+		},
+		clearUser: (state) => {
+			state.currentUser = null;
+		},
 	},
-    clearUser: (state) => {
-		state.currentUser = null;
-	},
-  },
 });
 
-export const { setUser, clearUser } = userSlice.actions;
+export const { loginSuccess, loginError, clearUser } = userSlice.actions;
 
-export default userSlice.reducer; 
+export default userSlice.reducer;
