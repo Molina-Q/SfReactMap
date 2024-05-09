4ce4338ef1b14c691c9e071898eaafaaf69354a7
Molina-Q <quentinmolina1@gmail.com>
2024-05-09 13:14:19 +0200

    feat: Add API endpoint to retrieve users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +16,33 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'user' => $this->getUser(),
         ]);
+    }
+
+    #[Route('/api/getUsers', name: 'get_users', methods: ['GET'])]
+    public function getUsers(
+        UserRepository $userRepository
+
+    ): Response
+    {
+        $users = $userRepository->findAll();
+        $userArray = [];
+        foreach ($users as $user) {
+            $userArray[] = [
+                "id" => $user->getId(),
+                "email" => $user->getEmail(),
+                "roles" => $user->getRoles(),
+                "password" => $user->getPassword(),
+                "username" => $user->getUsername(),
+                "comments" => array_map(function($comment) { return $comment->getId(); }, $user->getComments()->toArray()),
+                "messages" => array_map(function($message) { return $message->getId(); }, $user->getMessages()->toArray()),
+                "topics" => array_map(function($topic) { return $topic->getId(); }, $user->getTopics()->toArray()),
+                "articles" => array_map(function($article) { return $article->getId(); }, $user->getArticles()->toArray()),
+                "articleEditeds" => array_map(function($articleEdited) { return $articleEdited->getId(); }, $user->getArticleEditeds()->toArray()),
+                "articleLikes" => array_map(function($articleLike) { return $articleLike->getId(); }, $user->getArticleLikes()->toArray()),
+                "isVerified" => $user->isVerified()
+            ];
+        }
+
+        return $this->json($userArray, 200);
     }
 }
