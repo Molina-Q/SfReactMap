5aa05527371ca723909f3fb279117cb2eda3dd34
Molina-Q <quentinmolina1@gmail.com>
2024-05-11 11:35:40 +0200

    refactor: Added useEffect to add the possibilty to change the content of page

--- assets/pages/Profile.jsx
+++ assets/pages/Profile.jsx
@@ -1,18 +1,39 @@
 import { jwtDecode } from "jwt-decode";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 export default function Profile() {
 	const {currentUser} = useSelector(state => state.user);
+	const [tab, setTab] = useState("");
+	const location = useLocation();
 
-	console.log("current user : ", currentUser.username);
 
-	// const token = currentUser.token;
-	// const decodedToken = jwtDecode(token);
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const tabFromUrl = urlParams.get("tab");
+		if (tabFromUrl) {
+			setTab(tabFromUrl);
+		}
+	}, [location.search]);
 
-	// console.log(decodedToken);
+	return (
+		<div className="min-h-screen flex flex-col md:flex-row">
+			<div className="md:w-56">
+				{/* Sidebar */}
+				<Sidebar />
+			</div>
 
-	return <div>
-        <h2>Welcome, {`${currentUser.username}`}</h2>
-    </div>;
+			{/* Profile */}
+			{tab === "profile" && <DashProfile />}
+
+			{/* Posts */}
+			{tab === "posts" && <DashPosts />}
+
+			{/* Users */}
+			{tab === "users" && <DashUsers />}
+		</div>
+	);
+
 }
