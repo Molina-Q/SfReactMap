8cd84ab27abcf3a35d990e6f2dd5569fba2cdc42
Molina-Q <quentinmolina1@gmail.com>
2024-05-11 12:59:50 +0200

    Create profile article component

---
+++ assets/components/profileTabs/ProfileArticles.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function ProfileArticles() {
+  return (
+    <div>
+      <Link to='/article/create'>
+        <button>Create an article</button>
+      </Link>
+      ProfileArticles
+
+
+    </div>
+  )
+}
