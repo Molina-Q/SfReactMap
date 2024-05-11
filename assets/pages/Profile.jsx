0bef69b9c2c732f909f76c64bb810c1086686772
Molina-Q <quentinmolina1@gmail.com>
2024-05-11 13:00:40 +0200

    refactor: Update profile page sidebar styles and structure

--- assets/pages/Profile.jsx
+++ assets/pages/Profile.jsx
@@ -3,11 +3,16 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import ProfileUser from "../components/profileTabs/ProfileUser";
+import ProfileArticle from "../components/profileTabs/ProfileArticles";
+import ProfileArticles from "../components/profileTabs/ProfileArticles";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
 	const {currentUser} = useSelector(state => state.user);
 	const [tab, setTab] = useState("");
 	const location = useLocation();
+	const navigate = useNavigate();
 
 
 	useEffect(() => {
@@ -15,25 +20,31 @@ export default function Profile() {
 		const tabFromUrl = urlParams.get("tab");
 		if (tabFromUrl) {
 			setTab(tabFromUrl);
+		} else {
+			const urlParams = new URLSearchParams();
+			urlParams.set("tab", 'user');
+
+			const searchQuery = urlParams.toString();
+			// setTab('user');
+			navigate(`/profile?${searchQuery}`);			
 		}
 	}, [location.search]);
 
 	return (
-		<div className="min-h-screen flex flex-col md:flex-row">
-			<div className="md:w-56">
-				{/* Sidebar */}
+		<main className="wrapperMain wrap-equip wrap-profile">
+			<div className="sidebar-container">
 				<Sidebar />
 			</div>
 
 			{/* Profile */}
-			{tab === "profile" && <DashProfile />}
+			{tab === "user" && <ProfileUser />}
 
 			{/* Posts */}
-			{tab === "posts" && <DashPosts />}
+			{tab === "articles" && <ProfileArticles />}
 
 			{/* Users */}
 			{tab === "users" && <DashUsers />}
-		</div>
+		</main>
 	);
 
 }
