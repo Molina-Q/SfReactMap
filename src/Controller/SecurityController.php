292d6a3fee9ae2f09998c8cce8644096c9c47e5b
Molina-Q <quentinmolina1@gmail.com>
2024-05-14 16:59:05 +0200

    refactor: Added signin method for test purpose

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
-
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 
@@ -45,20 +48,70 @@ class SecurityController extends AbstractController
         return $response;
     }
 
+    #[Route(path: '/api/login', name: 'app_login_check')]
+    public function signin(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepository,
+        JWTTokenManagerInterface $JWTManager,
+    ) {
+        $data = json_decode($request->getContent(), true);
+
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $user = $userRepository->findByEmail($email);
+
+        if (!$user) {
+            throw new AuthenticationException('User not found!');
+        }
+
+        $isValid = $passwordHasher->isPasswordValid($user, $password);
+
+        if (!$isValid) {
+            throw new AuthenticationException('Wrong credentials!');
+        }
+
+        $token = $JWTManager->create($user);
+        return new JsonResponse(['token' => $JWTManager->create($user)]);
+        $response = new JsonResponse();
+        // $response->headers->set('Authorization', "Bearer $token");
+        $response->headers->setCookie(
+            new Cookie(
+                'BEARER',
+                $token,
+                new \DateTime('+1 day'),
+                '/',
+                null,
+                true,
+                true,
+                false,
+                'strict'
+            )
+        );
+        $response->setData([
+            'username' => $user->getUsername(),
+            'id' => $user->getId(),
+            // add more user data here
+        ]);
+
+        return $response;
+    }
+
     #[Route("/api/user/getuser", name: 'get_user')]
     public function getConnectedUser(): JsonResponse
     {
         $userObj = $this->getUser();
 
         $token = $this->tokenStorageInterface->getToken();
-        
+
         // Add this check to ensure that a token exists
         if (!$token) {
             return $this->json(['error' => true, 'message' => 'No token found']);
         }
-        
+
         $userObj = $this->jwtManager->decode($this->tokenStorageInterface->getToken());
-        
+
         try {
             if ($userObj) {
                 $user = [
