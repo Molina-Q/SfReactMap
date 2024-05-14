6be7edb1e53cf0869ba3dad50fdef2a4c7cddd2a
Molina-Q <quentinmolina1@gmail.com>
2024-05-14 16:26:05 +0200

    refactor: Added content to ProfileUser component to fetch user data from API

--- assets/components/profileTabs/ProfileUser.jsx
+++ assets/components/profileTabs/ProfileUser.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function ProfileUser() {
-  return (
-    <div>ProfileUser</div>
-  )
+	const { currentUser } = useSelector((state) => state.user);
+	const [user, setUser] = useState(null);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		async function fetchUser() {
+			try {
+				const res = await fetch("/api/user/getuser");
+				const data = await res.json();
+				if (res.ok || data.error === 'false') {
+					console.log(data);
+					setUser(data);
+				}
+
+				if(data.error === true) {
+					setError(data.message);
+					setUser(null);
+				}
+				console.log("Response data : ", data);
+				console.log('user : ', user);
+			} catch (error) {
+				console.log("Error fetching user : ", error.message);
+				setUser(false);
+			}
+		}
+
+		fetchUser();
+	}, []);
+
+	return (
+		<div>
+			ProfileUser
+			<p>Hello, {currentUser.username} </p>
+			{error && <p>{error}</p>}
+			{user && (
+				<div>
+					<ul>
+						<li>{user.username}</li>
+						<li>{user.id}</li>
+						{/* <li>{user.roles.toString()}</li> */}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
