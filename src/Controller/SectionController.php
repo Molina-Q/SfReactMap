0db105886a157c4002c5ea193e67a7ef2fff37cb
Molina-Q <quentinmolina1@gmail.com>
2024-05-25 18:52:12 +0200

    Completed Back createSection functionnality

--- src/Controller/SectionController.php
+++ src/Controller/SectionController.php
@@ -2,86 +2,113 @@
 
 namespace App\Controller;
 
+use App\Entity\EquipmentSection;
 use App\Entity\Section;
 use App\Form\SectionFormType;
 use App\Repository\ArticleRepository;
 use App\Repository\SectionRepository;
+use App\Repository\EquipmentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWSProvider\JWSProviderInterface;
 
 class SectionController extends AbstractController
 {
-    #[Route('/section/create/{idArticle}', name: 'create_section')]
+    #[Route('/api/section/create/{idArticle}', name: 'create_section')]
     public function index(
         int $idArticle,
         ArticleRepository $articleRepository,
         SectionRepository $sectionRepository,
         EntityManagerInterface $entityManager,
-        Request $request
-    ): Response
-    {
-        $article = $articleRepository->findOneById($idArticle);
-
-        $section = new Section();
-        $section->setArticle($article);
-
-        $user = $this->getUser(); // get the user currently logged in
-
-        $form = $this->createForm(SectionFormType::class, $section, ['attr' => ['class' => 'form-create']]);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()) {
-
-            $entityManager->persist($section);
-            $entityManager->flush();
-
-            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
-
-            return $this->redirectToRoute('app_map');
-        }
-
-        return $this->render('map/createSection.html.twig', [
-            'createSectionForm' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/section/edit/{idSection}', name: 'edit_section')]
-    public function edit(
-        ArticleRepository $articleRepository,
-        SectionRepository $sectionRepository,
-        EntityManagerInterface $entityManager,
         Request $request,
-        int $idSection,
+        JWSProviderInterface $jwsProvider,
+        EquipmentRepository $equipmentRepository,
     ): Response
     {
-        $section = $sectionRepository->findOneById($idSection);
+        $requestData = json_decode($request->getContent(), true);
 
-        $form = $this->createForm(SectionFormType::class, $section, ['attr' => ['class' => 'form-create']]);
-        $form->handleRequest($request);
+        if(isset($requestData)) {
+            $tokenGet = $request->cookies->get('BEARER');
+            $token = filter_var($tokenGet, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    
+            $jws = $jwsProvider->load($token);
+            $decodedToken = $jws->getPayload();
+            $userId = $decodedToken['userId'];
 
-        if($form->isSubmitted() && $form->isValid()) {
+            $title = filter_var($requestData['title'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $text = filter_var($requestData['text'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $summary = filter_var($requestData['summary'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $equipmentSections = $requestData['equipmentSections']; 
+
+    
+            $id = filter_var($idArticle, FILTER_SANITIZE_NUMBER_INT);
+    
+            $article = $articleRepository->findOneById($id);
+    
+            $section = new Section();
+            $section->setTitle($title);
+            $section->setText($text);
+            $section->setSummary($summary);
+
+            $section->setArticle($article);
+
+            
+            foreach($equipmentSections as $equipmentSection) {
+                $equipment = $equipmentRepository->findOneById($equipmentSection['equip']);
+
+                $equipmentSect = new EquipmentSection();
+                $equipmentSect->setSection($section);
+                $equipmentSect->setEquipment($equipment);
+
+                $section->addEquipmentSection($equipmentSect);
+            }
 
             $entityManager->persist($section);
             $entityManager->flush();
 
             // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
 
-            return $this->redirectToRoute('app_map');
+            return $this->json([
+                'error' => false,
+                'message' => 'Section created',
+            ], 201);
         }
 
-        return $this->render('map/createSection.html.twig', [
-            'createSectionForm' => $form->createView(),
-        ]);
+        return $this->json([
+            'error' => true,
+            'message' => 'Section not created',
+        ], 400);
     }
 
-    #[Route('/section/delete/{id}', name: 'delete_section')]
-    public function delete(): Response
-    {
-        return $this->render('section/index.html.twig', [
-            'controller_name' => 'SectionController',
-        ]);
-    }
+    // #[Route('/section/edit/{idSection}', name: 'edit_section')]
+    // public function edit(
+    //     ArticleRepository $articleRepository,
+    //     SectionRepository $sectionRepository,
+    //     EntityManagerInterface $entityManager,
+    //     Request $request,
+    //     int $idSection,
+    // ): Response
+    // {
+    //     $section = $sectionRepository->findOneById($idSection);
+
+    //     $form = $this->createForm(SectionFormType::class, $section, ['attr' => ['class' => 'form-create']]);
+    //     $form->handleRequest($request);
+
+    //     if($form->isSubmitted() && $form->isValid()) {
+
+    //         $entityManager->persist($section);
+    //         $entityManager->flush();
+
+    //         // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
+
+    //         return $this->redirectToRoute('app_map');
+    //     }
+
+    //     return $this->render('map/createSection.html.twig', [
+    //         'createSectionForm' => $form->createView(),
+    //     ]);
+    // }
 }
