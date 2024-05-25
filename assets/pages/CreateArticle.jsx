d276cce4f3ab838fed18d8c60b2b977d264efc8a
Molina-Q <quentinmolina1@gmail.com>
2024-05-25 17:33:09 +0200

    Completed CreateArticle Form

--- assets/pages/CreateArticle.jsx
+++ assets/pages/CreateArticle.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateArticle() {
 	const [formData, setFormData] = useState({
-		title: "",
-		summary: "",
-		century: "",
-		country: "",
-		sections: [],
+		'title': '',
+		'summary': '',
+		'century': '',
+		'country': '',
 	});
+
+	const [centuries, setCenturies] = useState([]);
+	const [countries, setCountries] = useState([]);
+
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	const navigate = useNavigate();
 
 	const handleChange = (e) => {
 		setFormData({
@@ -15,24 +23,68 @@ export default function CreateArticle() {
 			[e.target.name]: e.target.value,
 		});
 	};
+	console.log(formData);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-	const handleSectionChange = (index, event) => {
-		const newSections = [...formData.sections];
-		newSections[index] = event.target.value;
-		setFormData({
-			...formData,
-			sections: newSections,
-		});
+		const values = Object.values(formData);
+		if (values.every(value => value === '')) {
+		  setError('Please fill out all fields.');
+		  return;
+		}
+
+		try {
+			const response = await fetch("/api/article/create", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+
+			const data = await response.json();
+
+			if (!response.ok) {
+				setError(data.message);
+			}
+			// Handle successful form submission here
+			// For example, you can clear the form
+			setFormData({});
+			navigate("/section/create");
+		} catch (error) {
+			setError(error.message);
+		}
 	};
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		// Handle form submission here
-	};
+	useEffect(() => {
+		const fetchCenturiesAndCountries = async () => {
+			setLoading(true);
+			try {
+				const res = await fetch("/api/article/data");
+
+				const data = await res.json();
+
+				if (res.ok) {
+					setCenturies(data.centuries);
+					setCountries(data.countries);
+
+					setLoading(false);
+				} else {
+					setLoading(false);
+					throw new Error(data.message);
+				}
+			} catch (error) {
+				console.log(error.message);
+				setLoading(false);
+			}
+		};
+
+		fetchCenturiesAndCountries();
+	}, []);
 
 	return (
 		<main className="wrap-login">
-            <h1>Write an article</h1>
+			<h1>Write an article</h1>
 			<form onSubmit={handleSubmit} className="form-create">
 				<div>
 					<label>Title</label>
@@ -41,6 +93,7 @@ export default function CreateArticle() {
 						name="title"
 						value={formData.title}
 						onChange={handleChange}
+						required
 					/>
 				</div>
 
@@ -50,6 +103,7 @@ export default function CreateArticle() {
 						name="summary"
 						value={formData.summary}
 						onChange={handleChange}
+						required
 					/>
 				</div>
 
@@ -59,9 +113,16 @@ export default function CreateArticle() {
 						name="century"
 						value={formData.century}
 						onChange={handleChange}
-						disabled
+						disabled={loading}
+						required
 					>
-						{/* Map over your century options here */}
+						<option value="none">None selected</option>
+
+						{centuries.map((century) => (
+							<option key={century.id} value={century.id}>
+								{century.year}
+							</option>
+						))}
 					</select>
 				</div>
 
@@ -71,25 +132,24 @@ export default function CreateArticle() {
 						name="country"
 						value={formData.country}
 						onChange={handleChange}
+						disabled={loading}
+						required
 					>
-						{/* Map over your country options here */}
+						<option value="none">None selected</option>
+
+						{countries.map((country) => (
+							<option key={country.id} value={country.id}>
+								{country.name}
+							</option>
+						))}
 					</select>
 				</div>
 
-				{formData.sections.map((section, index) => (
-					<div key={index}>
-						<label>Section {index + 1}</label>
-						<input
-							type="text"
-							name={`section${index}`}
-							value={section}
-							onChange={(event) => handleSectionChange(index, event)}
-						/>
-					</div>
-				))}
-
-				<button type="submit" className="form-btn-submit">Submit</button>
+				<button type="submit" disabled={loading} className="form-btn-submit">
+					Submit and Create a Section
+				</button>
 			</form>
+			{error && <p className="failure">{error}</p>}
 		</main>
 	);
 }
