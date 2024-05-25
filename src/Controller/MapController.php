236b144bbde0d325d01fb81ab965196a2ef5a1cf
Molina-Q <quentinmolina1@gmail.com>
2024-05-25 22:49:22 +0200

    Commented unused methods

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -21,89 +21,89 @@ class MapController extends AbstractController
 {
 
 
-    #[Route('/map/create/article/{countryStr}/{centuryStr}', name: 'create_article')]
-    public function createArticle(
-        Request $request,
-        CountryRepository $countryRepository,
-        CenturyRepository $centuryRepository,
-        EntityManagerInterface $entityManager,
-        string $countryStr,
-        string $centuryStr
-    ):Response
-    {
-        $country = $countryRepository->findOneByName($countryStr);
-        $century = $centuryRepository->findOneByYear($centuryStr);
-        $article = new Article();
-        $article->setCountry($country);
-        $article->setCentury($century);
+    // #[Route('/map/create/article/{countryStr}/{centuryStr}', name: 'create_article')]
+    // public function createArticle(
+    //     Request $request,
+    //     CountryRepository $countryRepository,
+    //     CenturyRepository $centuryRepository,
+    //     EntityManagerInterface $entityManager,
+    //     string $countryStr,
+    //     string $centuryStr
+    // ):Response
+    // {
+    //     $country = $countryRepository->findOneByName($countryStr);
+    //     $century = $centuryRepository->findOneByYear($centuryStr);
+    //     $article = new Article();
+    //     $article->setCountry($country);
+    //     $article->setCentury($century);
 
-        $user = $this->getUser(); // get the user currently logged in
+    //     $user = $this->getUser(); // get the user currently logged in
 
-        $form = $this->createForm(ArticleFormType::class, $article, ['attr' => ['class' => 'form-create']]);
-        $form->handleRequest($request);
+    //     $form = $this->createForm(ArticleFormType::class, $article, ['attr' => ['class' => 'form-create']]);
+    //     $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $dateNow = new \DateTime('now');
+    //     if($form->isSubmitted() && $form->isValid()) {
+    //         $dateNow = new \DateTime('now');
 
-            $article->setCreationDate($dateNow);
-            $article->setAuthor($user);
+    //         $article->setCreationDate($dateNow);
+    //         $article->setAuthor($user);
 
-            $entityManager->persist($article);
-            $entityManager->flush();
+    //         $entityManager->persist($article);
+    //         $entityManager->flush();
 
-            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
+    //         // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
 
-            return $this->redirectToRoute('app_map');
-        }
+    //         return $this->redirectToRoute('app_map');
+    //     }
 
-        return $this->render('map/createArticle.html.twig', [
-            'createArticleForm' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('map/createArticle.html.twig', [
+    //         'createArticleForm' => $form->createView(),
+    //     ]);
+    // }
 
-    #[Route('/map/edit/article/{id}', name: 'edit_article')]
-    public function editArticle(
-        Request $request,
-        ArticleRepository $articleRepository,
-        EntityManagerInterface $entityManager,
-        int $id
-    ):Response
-    {
+    // #[Route('/map/edit/article/{id}', name: 'edit_article')]
+    // public function editArticle(
+    //     Request $request,
+    //     ArticleRepository $articleRepository,
+    //     EntityManagerInterface $entityManager,
+    //     int $id
+    // ):Response
+    // {
  
-        $article = $articleRepository->findOneById($id);
+    //     $article = $articleRepository->findOneById($id);
 
-        $form = $this->createForm(ArticleFormType::class, $article, ['attr' => ['class' => 'form-create']]);
-        $form->handleRequest($request);
+    //     $form = $this->createForm(ArticleFormType::class, $article, ['attr' => ['class' => 'form-create']]);
+    //     $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+    //     if($form->isSubmitted() && $form->isValid()) {
 
-            $entityManager->persist($article);
-            $entityManager->flush();
+    //         $entityManager->persist($article);
+    //         $entityManager->flush();
 
-            // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
+    //         // $this->addFlash('success', 'The Topic '.$topic->getTitle().' was successfully updated');
 
-            return $this->redirectToRoute('app_map');
-        }
+    //         return $this->redirectToRoute('app_map');
+    //     }
 
-        return $this->render('map/createArticle.html.twig', [
-            'createArticleForm' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('map/createArticle.html.twig', [
+    //         'createArticleForm' => $form->createView(),
+    //     ]);
+    // }
 
-    #[Route('/map/delete/article/{id}', name: 'delete_article')]
-    public function deleteArticle(
-        Request $request,
-        ArticleRepository $articleRepository,
-        EntityManagerInterface $entityManager,
-        int $id
-    ):Response
-    {
+    // #[Route('/map/delete/article/{id}', name: 'delete_article')]
+    // public function deleteArticle(
+    //     Request $request,
+    //     ArticleRepository $articleRepository,
+    //     EntityManagerInterface $entityManager,
+    //     int $id
+    // ):Response
+    // {
  
-        $article = $articleRepository->findOneById($id);
+    //     $article = $articleRepository->findOneById($id);
 
-        $entityManager->remove($article);
-        $entityManager->flush();
+    //     $entityManager->remove($article);
+    //     $entityManager->flush();
         
-        return $this->redirectToRoute('app_map');
-    }
+    //     return $this->redirectToRoute('app_map');
+    // }
 }
