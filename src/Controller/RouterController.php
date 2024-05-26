bb3d305d4a0eeb2c74827ef73c832d472642a0ab
Molina-Q <quentinmolina1@gmail.com>
2024-05-26 15:23:50 +0200

    feat: Add routes for forum, topics, and topic creation/editing

--- src/Controller/RouterController.php
+++ src/Controller/RouterController.php
@@ -16,16 +16,21 @@ class RouterController extends AbstractController
     }
 
     #[Route('/home', name: 'app_home', methods: ['GET'])]
+    #[Route('/forum', name: 'app_forum', methods: ['GET'])]
+    #[Route('/forum/topics', name: 'topics_forum', methods: ['GET'])]
+    #[Route('/topic/create', name: 'show_create_topic', methods: ['GET'])]
+    #[Route('/topic/edit/{topicId}', name: 'show_edit_topic', methods: ['GET'])]
+
     #[Route('/equipment', name: 'app_equipment', methods: ['GET'])]
     #[Route('/equipment/create', name: 'show_create_equipment', methods: ['GET'])]
     #[Route('/equipment/edit/{id}', name: 'show_create_equipment', methods: ['GET'])]
+
     #[Route('/article/create', name: 'show_create_article', methods: ['GET'])]
     #[Route('/article/edit/{id}', name: 'show_create_article', methods: ['GET'])]
     #[Route('/section/create', name: 'show_create_section', methods: ['GET'])]
     #[Route('/section/edit/{id}', name: 'show_edit_section', methods: ['GET'])]
     #[Route('/map', name: 'app_map', methods: ['GET'])]
-    #[Route('/forum', name: 'app_forum', methods: ['GET'])]
-    #[Route('/forum/topics', name: 'topics_forum', methods: ['GET'])]
+
     #[Route('/profile', name: 'app_profile', methods: ['GET'])]
     public function index(): Response
     {
