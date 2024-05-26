b8918caaecb7e5e22f6c0e976142374f7703f720
Molina-Q <quentinmolina1@gmail.com>
2024-05-26 15:23:29 +0200

    refactor: Update CreateTopic form to handle Equipment and Article selection

--- assets/forms/CreateTopic.jsx
+++ assets/forms/CreateTopic.jsx
@@ -1,23 +1,122 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateTopic() {
-	const [formData, setformData] = useState({});
+	const [formData, setFormData] = useState({
+		title: "",
+		Equipment: "",
+		Article: "",
+		Message: "",
+	});
 
-	const handleChange = (e) => {
-		setFormState({
-			...formData,
-			[e.target.name]: e.target.value,
-		});
+	const [articles, setArticles] = useState([
+		{
+			id: "",
+			label: "",
+		},
+	]);
+
+	const [equipments, setEquipments] = useState([
+		{
+			id: "",
+			label: "",
+		},
+	]);
+
+	const [loading, setLoading] = useState(true);
+	const [loadingCategory, setLoadingCategory] = useState(true);
+	const [error, setError] = useState(null);
+
+	const navigate = useNavigate();
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+		const labels = ["Equipment", "Article"]
+
+		// if the value of e.target.name appear even once inside labels then it is true 
+		if (labels.some((label) => label === name)) {
+
+			setFormData(() => ({
+				...formData,
+				Equipment: name === "Equipment" ? value : "",
+				Article: name === "Article" ? value : "",
+			}));
+
+		} else {
+
+			setFormData(() => ({
+				...formData,
+				[e.target.name]: e.target.value,
+			}));
+		}
+	}
+
+	console.log(formData);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const values = Object.values(formData);
+		if (values.every((value) => value === "")) {
+			setError("Please fill out all fields.");
+			return;
+		}
+
+		try {
+			const response = await fetch("/api/forum/topic/create", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+
+			const data = await response.json();
+
+			if (!response.ok) {
+				setError(data.message);
+			}
+			// Handle successful form submission here
+			// For example, you can clear the form
+			setFormData({});
+			navigate("/forum/topics");
+		} catch (error) {
+			setError(error.message);
+		}
 	};
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		// Handle form submission here
-	};
+	useEffect(() => {
+		const fetchArticles = async () => {
+			try {
+				const response = await fetch(`/api/articles/equipments/data`, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				});
+
+				const data = await response.json();
+
+				if (response.ok) {
+					console.log(data);
+					setEquipments(data.equipments);
+					setArticles(data.articles);
+				} else {
+					console.error("Error:", data.message);
+				}
+
+				setLoadingCategory(false);
+			} catch (error) {
+				console.error("Error:", error);
+				setLoadingCategory(false);
+			}
+		};
+		fetchArticles();
+	}, []);
 
 	return (
 		<main className="wrapperMain wrap-login">
-            <h1>Write a Topic</h1>
+			<h1>Write a Topic</h1>
 			<form onSubmit={handleSubmit} className="form-create">
 				<div>
 					<label>Title</label>
@@ -33,40 +132,50 @@ export default function CreateTopic() {
 				<div>
 					<label>Equipment</label>
 					<select
-						name="equipment"
-						value={formData.equipment}
+						name="Equipment"
+						value={formData.Equipment}
 						onChange={handleChange}
 						className="form-input-select"
+						disabled={loadingCategory}
 					>
-						{/* Map over your equipment options here */}
+						<option value="">None</option>
+						{equipments.map((equipment) => (
+							<option key={equipment.id} value={equipment.id}>
+								{equipment.label}
+							</option>
+						))}
 					</select>
 				</div>
 
 				<div>
 					<label>Article</label>
 					<select
-						name="article"
-						value={formData.article}
+						name="Article"
+						value={formData.Article}
 						onChange={handleChange}
 						className="form-input-select"
+						disabled={loadingCategory}
 					>
-						{/* Map over your article options here */}
+						<option value="">None</option>
+						{articles.map((article) => (
+							<option key={article.id} value={article.id}>
+								{article.label}
+							</option>
+						))}
 					</select>
 				</div>
 
 				<div>
 					<label>Message Author</label>
 					<textarea
-						name="msgAuthor"
+						name="Message"
 						value={formData.msgAuthor}
 						onChange={handleChange}
 						className="form-input-text"
 					/>
 				</div>
 
-				<button type="submit" className="form-btn-submit">
-					Submit
-				</button>
+				<button className="form-btn-submit">Submit</button>
 			</form>
 		</main>
 	);
