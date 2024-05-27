ee979398e753babace24e288bc82644903eee1c2
Molina-Q <quentinmolina1@gmail.com>
2024-05-27 16:36:54 +0200

    refactor: Add isLink prop to SingleImg component for conditional rendering of links

--- assets/components/gallery/SingleImg.jsx
+++ assets/components/gallery/SingleImg.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function SingleImg({ item, className, clickEvent }) {
-  // path to the picture of the item
-  const pathImg = `/img/upload/${item.img}`;
+function SingleImg({ item, className, clickEvent, isLink}) {
+	// path to the picture of the item
+	const pathImg = `/img/upload/${item.img}`;
 
-  return (
-    <figure key={item.id} className={className} onClick={()=>clickEvent(item.id)}>
-      <img src={pathImg} alt={item.name} />
+	return (
+		<figure
+			key={item.id}
+			className={className}
+			onClick={() => clickEvent(item.id)}
+		>
+			<img src={pathImg} alt={item.name} />
 
-      <figcaption>
-        <a href={ item.url ? item.url : "/forum"}>{item.name}</a>
-      </figcaption>
-    </figure>
-  );
+			<figcaption>
+				{isLink ? <Link className="figcaption-item" to={`/equipment?item=${item.id}`}>{item.name}</Link> : <p className="figcaption-item">{item.name}</p>}
+			</figcaption>
+		</figure>
+	);
 }
 
 export default SingleImg;
