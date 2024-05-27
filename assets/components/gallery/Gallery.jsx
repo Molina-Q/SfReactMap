1b209d5202fcbad3033d5b791db60fe5ca4df411
Molina-Q <quentinmolina1@gmail.com>
2024-05-27 16:36:31 +0200

    refactor: Update Gallery component to include isLink prop for conditional rendering of links

--- assets/components/gallery/Gallery.jsx
+++ assets/components/gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SingleImg from "./SingleImg";
 
-function Gallery({ items, className, clickEvent }) {
+function Gallery({ items, className, clickEvent, isLink = false}) {
   return (
     <>
       {items.map((item) => (
-        <SingleImg key={item.id} item={item} className={className} url={`/api/equipment/${item.id}`} clickEvent={clickEvent} />
+        <SingleImg key={item.id} item={item} className={className} clickEvent={clickEvent} isLink={isLink} />
       ))}
     </>
   );
