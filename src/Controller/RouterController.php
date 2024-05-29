1ed5dbb59034689e51d1369a56c4d92819b80f2a
Molina-Q <quentinmolina1@gmail.com>
2024-05-29 16:48:53 +0200

    added public to all routes name route with public in the name do not need to be checked by verifyToken service

--- src/Controller/RouterController.php
+++ src/Controller/RouterController.php
@@ -9,40 +9,40 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RouterController extends AbstractController
 {
-    #[Route('/', name: 'app_index', methods: ['GET'])]
+    #[Route('/', name: 'public_app_index', methods: ['GET'])]
     public function indexRedirect(): Response
     {
         return $this->redirectToRoute('app_home');
     }
 
     // all of my page's routes
-    #[Route('/home', name: 'app_home', methods: ['GET'])]
-    #[Route('/forum', name: 'app_forum', methods: ['GET'])]
-    #[Route('/forum/topic/{id}', name: 'one_topic_forum', methods: ['GET'])]
-    #[Route('/forum/topics', name: 'all_topics_forum', methods: ['GET'])]
-    #[Route('/topic/create', name: 'show_create_topic', methods: ['GET'])]
+    #[Route('/home', name: 'public_app_home', methods: ['GET'])]
+    #[Route('/forum', name: 'public_app_forum', methods: ['GET'])]
+    #[Route('/forum/topic/{id}', name: 'public_one_topic_forum', methods: ['GET'])]
+    #[Route('/forum/topics', name: 'public_all_topics_forum', methods: ['GET'])]
+    #[Route('/topic/create', name: 'public_show_create_topic', methods: ['GET'])]
     #[Route('/topic/edit/{topicId}', name: 'show_edit_topic', methods: ['GET'])]
 
-    #[Route('/equipment', name: 'app_equipment', methods: ['GET'])]
-    #[Route('/equipment/create', name: 'show_create_equipment', methods: ['GET'])]
-    #[Route('/equipment/edit/{id}', name: 'show_edit_equipment', methods: ['GET'])]
+    #[Route('/equipment', name: 'public_app_equipment', methods: ['GET'])]
+    #[Route('/equipment/create', name: 'public_show_create_equipment', methods: ['GET'])]
+    #[Route('/equipment/edit/{id}', name: 'public_show_edit_equipment', methods: ['GET'])]
 
-    #[Route('/article/create', name: 'show_create_article', methods: ['GET'])]
-    #[Route('/article/edit/{id}', name: 'show_edit_article', methods: ['GET'])]
+    #[Route('/article/create', name: 'public_show_create_article', methods: ['GET'])]
+    #[Route('/article/edit/{id}', name: 'public_show_edit_article', methods: ['GET'])]
 
-    #[Route('/section/create', name: 'show_create_section', methods: ['GET'])]
-    #[Route('/section/edit/{id}', name: 'show_edit_section', methods: ['GET'])]
+    #[Route('/section/create', name: 'public_show_create_section', methods: ['GET'])]
+    #[Route('/section/edit/{id}', name: 'public_show_edit_section', methods: ['GET'])]
 
-    #[Route('/map', name: 'app_map', methods: ['GET'])]
+    #[Route('/map', name: 'public_app_map', methods: ['GET'])]
 
-    #[Route('/profile', name: 'app_profile', methods: ['GET'])]
+    #[Route('/profile', name: 'public_app_profile', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('base.html.twig');
     }
 
-    #[Route('/login', name: 'app_show_login', methods: ['GET'])]
-    #[Route('/register', name: 'app_show_register', methods: ['GET'])]
+    #[Route('/login', name: 'public_app_show_login', methods: ['GET'])]
+    #[Route('/register', name: 'public_app_show_register', methods: ['GET'])]
     public function indexAuth(): Response
     {
         if($this->getUser()) {
