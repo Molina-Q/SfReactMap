3eef3c478450a7cdc740206a7d15a3f7a7f7206f
Molina-Q <quentinmolina1@gmail.com>
2024-05-29 11:53:30 +0200

    minor changes

--- assets/components/profileTabs/ProfileArticles.jsx
+++ assets/components/profileTabs/ProfileArticles.jsx
@@ -32,7 +32,6 @@ export default function ProfileArticles() {
 
 				console.log("Response data : ", data);
 				console.log("entityToDelete : ", entityToDelete);
-
 			} catch (error) {
 				console.log("Error fetching entityToDelete : ", error.message);
 			}
@@ -52,44 +51,43 @@ export default function ProfileArticles() {
 	const handleDeleteSection = () => {
 		// Here you can call your API to delete the entity
 		const fetchDeleteEntity = async () => {
-      try {
-        const res = await fetch(`/api/section/delete/${entityToDelete}`, {
-          method: "DELETE",
-        });
-        const data = await res.json();
+			try {
+				const res = await fetch(`/api/section/delete/${entityToDelete}`, {
+					method: "DELETE",
+				});
+				const data = await res.json();
 
-        if (res.ok) {
-          console.log(data);
-        }
-      } catch (error) {
-        console.log("Error deleting entity : ", error.message);
-      }
-    };
+				if (res.ok) {
+					console.log(data);
+				}
+			} catch (error) {
+				console.log("Error deleting entity : ", error.message);
+			}
+		};
 
-    fetchDeleteEntity();
+		fetchDeleteEntity();
 
 		closeDialog();
 	};
 
-  
 	const handleDeleteArticle = () => {
 		// Here you can call your API to delete the entity
 		const fetchDeleteEntity = async () => {
-      try {
-        const res = await fetch(`/api/article/delete/${entityToDelete}`, {
-          method: "DELETE",
-        });
-        const data = await res.json();
+			try {
+				const res = await fetch(`/api/article/delete/${entityToDelete}`, {
+					method: "DELETE",
+				});
+				const data = await res.json();
 
-        if (res.ok) {
-          console.log(data);
-        }
-      } catch (error) {
-        console.log("Error deleting entity : ", error.message);
-      }
-    };
+				if (res.ok) {
+					console.log(data);
+				}
+			} catch (error) {
+				console.log("Error deleting entity : ", error.message);
+			}
+		};
 
-    fetchDeleteEntity();
+		fetchDeleteEntity();
 
 		closeDialog();
 	};
@@ -98,15 +96,13 @@ export default function ProfileArticles() {
 		<div>
 			ProfileArticles
 			<h2>ARTICLE</h2>
-
 			<Link to="/article/create">
 				<button>Create an Article</button>
 			</Link>
-
 			<section>
 				{fetchedData.map((article) => (
 					<div key={article.id}>
-            <h2>{article.category}</h2>
+						<h2>{article.category}</h2>
 						<h3>{article.title}</h3>
 						<p>{article.summary}</p>
 						<Link to={`/article/edit/${article.id}`}>
@@ -121,7 +117,6 @@ export default function ProfileArticles() {
 					</div>
 				))}
 			</section>
-
 			{/* <Link to='#'> */}
 			{/* </Link> */}
 			<h2>SECTION</h2>
@@ -132,15 +127,10 @@ export default function ProfileArticles() {
 				<button>Edit a Section</button>
 			</Link>
 			{/* <Link to='#'> */}
-			<button
-				name=""
-				className="delete-btn"
-				onClick={() => openDialog(8)}
-			>
+			<button name="" className="delete-btn" onClick={() => openDialog(8)}>
 				Delete a Section
 			</button>
 			{/* </Link> */}
-
 			<dialog ref={dialogRef}>
 				<h2>Confirm Deletion</h2>
 				<p>Are you sure you want to delete this entity {entityToDelete} ?</p>
