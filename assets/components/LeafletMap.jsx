0dc74ac02644db8ae0955ac39ef494fc5eb3ab70
Molina-Q <quentinmolina1@gmail.com>
2024-06-01 20:50:12 +0200

    Added countriesWithArticles in order to gray countries without articles

--- assets/components/LeafletMap.jsx
+++ assets/components/LeafletMap.jsx
@@ -9,130 +9,160 @@ import myGeoJson1900 from "../geojson/1900countries.json";
 
 // all possible value for geoJSonFeatures
 const myGeoJsons = {
-  1400: myGeoJson1400,
-  1500: myGeoJson1500,
-  1600: myGeoJson1600,
-  1700: myGeoJson1700,
-  1800: myGeoJson1800,
-  1900: myGeoJson1900,
+	1400: myGeoJson1400,
+	1500: myGeoJson1500,
+	1600: myGeoJson1600,
+	1700: myGeoJson1700,
+	1800: myGeoJson1800,
+	1900: myGeoJson1900,
 };
 
 const LeafletMap = ({ checkedValue, handleClickOnCountry }) => {
-  const position = [46.2276, 3.2137];
+	const position = [46.2276, 3.2137];
 
-  // ce state sera géré par son parent SfReactMap
-  // const [checkedValue, setCheckedValue] = useState('1900') ;
+	const [countriesWithArticles, setCountriesWithArticles] = useState([]);
 
-  // géré ici car utilisé dans son Enfant GeoJsonGeometry
-  const [geoJsonFeatures, setGeoJsonFeatures] = useState(null);
-
-  // sera géré dans le parent
-  // const [data, setData] = useState(null);
-
-  useEffect(() => {
+	useEffect(() => {
     myGeoJsons[checkedValue]
-      ? setGeoJsonFeatures(myGeoJsons[checkedValue].features)
-      : setGeoJsonFeatures(myGeoJson1900.features);
+    ? setGeoJsonFeatures(myGeoJsons[checkedValue].features)
+    : setGeoJsonFeatures(myGeoJson1900.features);
 
-    // if (myGeoJsons[checkedValue]) {
-    //   setGeoJsonFeatures(myGeoJsons[checkedValue].features);
-    // } else {
-    //   setGeoJsonFeatures(myGeoJson1900.features);
-    // }
-  }, [checkedValue]); // this make it so useEffect will take effect only when checkedValue is changed
+		// Fetch all countries that have articles
+		const fetchCountriesWithArticles = async () => {
+			const response = await fetch(`/api/articles/countries/${checkedValue}`);
+			const data = await response.json();
 
-  // Style variable for the country polygon
-  const options = {
-    BEL: { color: "yellow", fillOpacity: 0.1 },
-    FRA: { color: "blue", fillOpacity: 0.1 },
-    DEU: { color: "green", fillOpacity: 0.1 }, // Germany
-    RUS: { color: "grey", fillOpacity: 0.1 },
-    GBR: { color: "purple", fillOpacity: 0.1 }, // UK
-    ITA: { color: "red", fillOpacity: 0.1 },
-    ESP: { color: "orange", fillOpacity: 0.1 },
-  };
+			if (data.error === false) {
+				setCountriesWithArticles(data.object);
+				return;
+			}
+		};
 
-  /**
-   * Function that loops on all of the countries in the json sent in params and return them as an array
-   * @param {JSON} jsonFeatures
-   */
-  function GeoJsonGeometry({ jsonFeatures }) {
-    const geoJSON = [];
+		fetchCountriesWithArticles();
+	}, [checkedValue]);
 
-    const map = useMap();
+	// ce state sera géré par son parent SfReactMap
+	// const [checkedValue, setCheckedValue] = useState('1900') ;
 
-    if (jsonFeatures) {
-      // loop on feature to get info on each individual country
-      for (let feature of jsonFeatures) {
-        let polygon;
+	// géré ici car utilisé dans son Enfant GeoJsonGeometry
+	const [geoJsonFeatures, setGeoJsonFeatures] = useState(null);
 
-        // if simple polygon
-        if (feature.geometry.type == "Polygon") {
-          // get the coordinates of the country in the correct order (latitude, longitude)
-          polygon = [
-            feature.geometry.coordinates[0].map((internalArrayItem) =>
-              internalArrayItem.toReversed()
-            ),
-          ];
+	// sera géré dans le parent
+	// const [data, setData] = useState(null);
 
-          // if multi-polygon, so has one more layer in the json file
-        } else {
-          polygon = feature.geometry.coordinates.map((externalArrayItem) => [
-            externalArrayItem[0].map((internalArrayItem) =>
-              internalArrayItem.toReversed()
-            ),
-          ]);
-        }
+	useEffect(() => {
 
-        // eventHandlers for the map
-        const handlers = useMemo(
-          () => ({
-            click() {
-              handleClickOnCountry(feature.properties.ADMIN);
-              map.panInsideBounds(polygon);
-            },
-          }),
-          [map]
-        );
 
-        // push each country's geometry in the array, and use their names as key
-        geoJSON.push(
-          <GeoJSON
-            data={feature.geometry}
-            key={feature.properties.ADMIN} // their names are used as key
-            pathOptions={options[feature.properties.ISO_A3]} // change the style variable depending on the country
-            eventHandlers={handlers}
-            smoothFactor={4}
-          />
-        );
+		// if (myGeoJsons[checkedValue]) {
+		//   setGeoJsonFeatures(myGeoJsons[checkedValue].features);
+		// } else {
+		//   setGeoJsonFeatures(myGeoJson1900.features);
+		// }
+
+		const optionsCopy = { ...options };
+		for (let countryCode in optionsCopy) {
+      console.log(countriesWithArticles);
+			if (!countriesWithArticles.includes(countryCode)) {
+				optionsCopy[countryCode].isGray = true;
+			} else {
+        optionsCopy[countryCode].isGray = false;
       }
-    }
+		}
+		setOptions(optionsCopy, countriesWithArticles);
+	}, [countriesWithArticles]); // this make it so useEffect will take effect only when checkedValue is changed
 
-    return geoJSON;
-  }
+	// Style variable for the country polygon
+	const [options, setOptions] = useState({
+		BEL: { color: "yellow", fillOpacity: 0.1, isGray: false},
+		FRA: { color: "blue", fillOpacity: 0.1, isGray: false },
+		DEU: { color: "green", fillOpacity: 0.1, isGray: false }, // Germany
+		RUS: { color: "green", fillOpacity: 0.1, isGray: false },
+		GBR: { color: "purple", fillOpacity: 0.1, isGray: false }, // UK
+		ITA: { color: "red", fillOpacity: 0.1, isGray: false },
+		ESP: { color: "orange", fillOpacity: 0.1, isGray: false },
+	});
 
-  return (
-    <div id="leafletMap">
-      <MapContainer
-        className="map"
-        center={position}
-        zoom={6}
-        scrollWheelZoom={true}
-        zoomControl={false}
-      >
-        {/* My map img */}
-        <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          minZoom={3}
-          maxZoom={8}
-        />
+	/**
+	 * Function that loops on all of the countries in the json sent in params and return them as an array
+	 * @param {JSON} jsonFeatures
+	 */
+	function GeoJsonGeometry({ jsonFeatures }) {
+		const geoJSON = [];
 
-        {/* Country polygon */}
-        <GeoJsonGeometry jsonFeatures={geoJsonFeatures} />
-      </MapContainer>
-    </div>
-  );
+		const map = useMap();
+
+		if (jsonFeatures) {
+			// loop on feature to get info on each individual country
+			for (let feature of jsonFeatures) {
+				let polygon;
+
+				// if simple polygon
+				if (feature.geometry.type == "Polygon") {
+					// get the coordinates of the country in the correct order (latitude, longitude)
+					polygon = [
+						feature.geometry.coordinates[0].map((internalArrayItem) =>
+							internalArrayItem.toReversed()
+						),
+					];
+
+					// if multi-polygon, so has one more layer in the json file
+				} else {
+					polygon = feature.geometry.coordinates.map((externalArrayItem) => [
+						externalArrayItem[0].map((internalArrayItem) =>
+							internalArrayItem.toReversed()
+						),
+					]);
+				}
+
+				// eventHandlers for the map
+				const handlers = useMemo(
+					() => ({
+						click() {
+							handleClickOnCountry(feature.properties.ADMIN);
+							map.panInsideBounds(polygon);
+						},
+					}),
+					[map]
+				);
+
+				// push each country's geometry in the array, and use their names as key
+				geoJSON.push(
+					<GeoJSON
+						data={feature.geometry}
+						key={feature.properties.ADMIN} // their names are used as key
+						pathOptions={options[feature.properties.ISO_A3].isGray ? { color: "gray", fillOpacity: 0.1 } : options[feature.properties.ISO_A3]} // Use gray if no options for this country
+						eventHandlers={handlers}
+						smoothFactor={4}
+					/>
+				);
+			}
+		}
+
+		return geoJSON;
+	}
+
+	return (
+		<div id="leafletMap">
+			<MapContainer
+				className="map"
+				center={position}
+				zoom={6}
+				scrollWheelZoom={true}
+				zoomControl={false}
+			>
+				{/* My map img */}
+				<TileLayer
+					url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+					minZoom={3}
+					maxZoom={8}
+				/>
+
+				{/* Country polygon */}
+				<GeoJsonGeometry jsonFeatures={geoJsonFeatures} />
+			</MapContainer>
+		</div>
+	);
 };
 
 export default LeafletMap;
