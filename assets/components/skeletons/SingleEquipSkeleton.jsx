ea8574cd23c1e441b1adc37fcb4ab2bb664ddd3d
Molina-Q <quentinmolina1@gmail.com>
2024-06-07 14:39:23 +0200

    feat: Created SingleEquipSkeleton component for displaying equipment details skeleton while loading

---
+++ assets/components/skeletons/SingleEquipSkeleton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SingleEquipSkeleton() {
+	return (
+		<div className="skeleton">
+			<div className="skeleton-text-sm"></div>
+			<div className="skeleton-text-lg"></div>
+			<div className="skeleton-image"></div>
+			<div className="skeleton-area"></div>
+		</div>
+	);
+}
