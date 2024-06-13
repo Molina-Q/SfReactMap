1d8cccdb0f6a9f71067f246da64702864520aebf
Molina-Q <quentinmolina1@gmail.com>
2024-06-13 14:19:06 +0200

    chore: Update RegistrationController with JSON validation and error handling

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,61 +35,73 @@ class RegistrationController extends AbstractController
     #[Route('/api/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
-        $data = json_decode($request->getContent(), true);
+        try {
+            $data = json_decode($request->getContent(), true);
+            // dd($data);
 
-        $constraints = new Collection([
-            'username' => [
-                new Length(['min' => 4, 'max' => 50]),
-            ],
-            'email' => [
-                new NotBlank(),
-                new Email()
-            ],
-            'password' => [
-                new Regex([
-                    'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/',
-                    'message' => 'Password must be at least 10 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character.'
-                ]),
-            ],
-        ]);
-    
-        $violations = $validator->validate($data, $constraints);
-    
-        if (count($violations) > 0) {
-            $errors = [];
-            foreach ($violations as $violation) {
-                $errors[$violation->getPropertyPath()] = $violation->getMessage();
+            $constraints = new Collection([
+                'username' => [
+                    new Length(['min' => 4, 'max' => 50]),
+                ],
+                'email' => [
+                    new NotBlank(),
+                    new Email()
+                ],
+                'password' => [
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/',
+                        'message' => 'Password must be at least 10 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character.'
+                    ]),
+                ],
+            ]);
+        
+            $violations = $validator->validate($data, $constraints);
+            
+
+            if (count($violations) > 0) {
+                $errors = [];
+                foreach ($violations as $violation) {
+                    $errors[$violation->getPropertyPath()] = $violation->getMessage();
+                    }
+                
+                return $this->json(['error' => true, 'fieldErrors' => $errors], 400);
             }
-    
-            return new JsonResponse(['message' => $errors], 400);
+
+            // If the data is valid, continue with registration
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setEmail($data['email']);
+
+            $date = new \DateTime();
+            $user->setCreationDate($date);
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $data['password']
+                )
+            );
+        
+            $entityManager->persist($user);
+            $entityManager->flush();
+        
+            // generate a signed url and email it to the user
+            // $this->emailVerifier->sendEmailConfirmation(
+            //     'app_verify_email',
+            //     $user,
+            //     (new TemplatedEmail())
+            //         ->from(new Address('mail@sfreactmap.com', 'Mail Bot'))
+            //         ->to($user->getEmail())
+            //         ->subject('Please Confirm your Email')
+            //         ->htmlTemplate('registration/confirmation_email.html.twig')
+            // );
+        
+            return $this->json(['error' => false, 'message' => 'User created'], 201);
+        } catch (\Throwable $th) {
+
+            return $this->json(['error' => true, 'message' => 'Something went wrong'], 500);
         }
-    
-        // If the data is valid, continue with registration
-        $user = new User();
-        $user->setUsername($data['username']);
-        $user->setEmail($data['email']);
-        $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                $user,
-                $data['password']
-            )
-        );
-    
-        $entityManager->persist($user);
-        $entityManager->flush();
-    
-        // generate a signed url and email it to the user
-        // $this->emailVerifier->sendEmailConfirmation(
-        //     'app_verify_email',
-        //     $user,
-        //     (new TemplatedEmail())
-        //         ->from(new Address('mail@sfreactmap.com', 'Mail Bot'))
-        //         ->to($user->getEmail())
-        //         ->subject('Please Confirm your Email')
-        //         ->htmlTemplate('registration/confirmation_email.html.twig')
-        // );
-    
-        return new JsonResponse(['message' => 'User created'], 201);
+        
     }
 
     #[Route('/verify/email', name: 'app_verify_email')]
