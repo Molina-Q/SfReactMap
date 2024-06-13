a5060f44cfbf801eafc57bba18f5bfa427eabd69
Molina-Q <quentinmolina1@gmail.com>
2024-06-13 14:18:23 +0200

    chore: Update Register component with default user data and response handling

--- assets/pages/Register.jsx
+++ assets/pages/Register.jsx
@@ -4,8 +4,14 @@ import Alert from "../components/UI/Alert";
 import Loading from "../components/UI/animation/Loading";
 
 export default function Register() {
-	const [userData, setUserData] = useState({}); // Initialize user state
-	const [dataMessage, setDataMessage] = useState(null); // Initialize dataMessage state
+	const [userData, setUserData] = useState({
+		username: 'micka',
+		email: 'micka@exemple.com',
+		password: 'Aaaaaaaa123@'
+	}); // Initialize user state
+
+	const [dataResponse, setDataResponse] = useState(null); // Initialize dataResponse state
+
 	const handleChange = (e) => {
 		setUserData({
 			...userData,
@@ -13,10 +19,12 @@ export default function Register() {
 		});
 	};
 
+	console.log(userData);
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
-		setDataMessage(null);
+		setDataResponse(null);
 
 		try {
 			const response = await fetch("/api/register", {
@@ -26,27 +34,43 @@ export default function Register() {
 				},
 				body: JSON.stringify(userData),
 			});
+
 			const data = await response.json();
 
-			setDataMessage(data.message);
+			if (response.ok) {
+				setDataResponse(data);
+			} else {
+				setDataResponse(data)
+			}
+
 		} catch (error) {
-			setDataMessage(error.message);
+			setDataResponse(error.message);
 		}
 	};
+
+	console.log('dataResponse : ', dataResponse);
 
 	return (
 		<main className="wrapperMain wrap-login">
 			<h1>Create an account</h1>
 			<form onSubmit={handleSubmit} className="form-create">
-				{/* Form fields here, e.g.: */}
+
+				{dataResponse && dataResponse.message &&
+					<p className={`alert ${dataResponse.error ? "failure" : "success"}`}>{dataResponse.message}</p>
+				}
+
 				<div>
 					<label htmlFor="username">Username</label>
 					<input
 						type="text"
 						name="username"
 						onChange={handleChange}
+						value={userData.username}
 						className="form-input-text"
 					/>
+					{dataResponse && dataResponse.fieldErrors["[username]"] &&
+						<p className={"alert failure"}>{dataResponse.fieldErrors["[username]"]}</p>
+					}
 				</div>
 
 				<div>
@@ -55,25 +79,38 @@ export default function Register() {
 						type="email"
 						name="email"
 						onChange={handleChange}
+						value={userData.email}
 						className="form-input-text"
 					/>
+					{dataResponse && dataResponse.fieldErrors["[email]"] &&
+						<p className={"alert failure"}>{dataResponse.fieldErrors["[email]"]}</p>
+					}
 				</div>
 
 				<div>
 					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						name="password"
-						onChange={handleChange}
-						className="form-input-text"
-					/>
+					<div className="form-password">
+						<input
+							type="password"
+							name="password"
+							onChange={handleChange}
+							value={userData.password}
+							className="form-input-text"
+						/>
+
+						<button type="button" onClick={toggleShowPassword} className="passwordToggle">
+							{showPassword ? <FaRegEyeSlash size={"15px"} /> : <FaRegEye />}
+						</button>
+					</div>
+					{dataResponse && dataResponse.fieldErrors["[password]"] &&
+						<p className={"alert failure"}>{dataResponse.fieldErrors["[password]"]}</p>
+					}
 				</div>
-				{/* Add other fields as necessary */}
+
 				<button className="form-btn-submit" onClick={handleSubmit}>
 					Submit
 				</button>
 			</form>
-			{dataMessage && <p>{dataMessage}</p>}
 		</main>
 	);
 }
