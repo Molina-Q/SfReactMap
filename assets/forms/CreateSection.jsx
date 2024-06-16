293d703904e53eb1e76b45f1795c152237d75449
Molina-Q <quentinmolina1@gmail.com>
2024-06-16 19:27:07 +0200

    refactor: Add react-helmet-async to CreateSection component

--- assets/forms/CreateSection.jsx
+++ assets/forms/CreateSection.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 
 export default function CreateSection() {
 	const [formState, setFormState] = useState({
