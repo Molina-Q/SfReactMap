bcc08af09cdc4f4da1806faa95e6c2d45dfbc6f1
Molina-Q <quentinmolina1@gmail.com>
2024-06-16 21:56:31 +0200

    refactor: Add ProfileAdmin tab to Profile page

--- assets/components/profileTabs/ProfileAdmin.jsx
+++ assets/components/profileTabs/ProfileAdmin.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ProfileAdmin() {
-  return (
-    <div>ProfileAdmin</div>
-  )
+    const style = {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        gap: "1rem"
+    };
+
+    const btnStyle = {
+        padding: "1rem",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+        fontSize: "1.2rem",
+    };
+    
+
+	return (
+		<div style={style}>
+			<Link to="/article/create" >
+				<button style={btnStyle}>Create an Article</button>
+			</Link>
+			<Link to="/section/create">
+				<button style={btnStyle}>Create a Section</button>
+			</Link>
+            <Link to="/equipment/create">
+				<button style={btnStyle}>Create an Equipment</button>
+			</Link>
+		</div>
+	);
 }
