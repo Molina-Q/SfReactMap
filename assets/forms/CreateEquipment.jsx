aa77cd43d0992edbc058368ea6613bf0b89a4fca
Molina-Q <quentinmolina1@gmail.com>
2024-06-16 19:27:04 +0200

    refactor: Add react-helmet-async to CreateEquipment component

--- assets/forms/CreateEquipment.jsx
+++ assets/forms/CreateEquipment.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 
 export default function CreateEquipment() {
 	const [formData, setFormData] = useState({
